da9c23bea160f6b2f1599f11b5aae09b562242ae
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-04 15:40:08 -0500

    Initial PIP package prep and testing

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="falconpy", # Replace with your own username
+    version="0.1.5",
+    author="CrowdStrike",
+    maintainer="Joshua Hiller",
+    description="The CrowdStrike Falcon API SDK for Python 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CrowdStrike/falconpy",
+    packages=setuptools.find_packages(),
+    install_requires=[
+	"requests",
+	"urllib3"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
