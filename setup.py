01946ec1d3ecfde44e54745a32eab05f1f8b431b
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-05 16:32:30 -0500

    Package updates

--- setup.py
+++ setup.py
@@ -1,30 +1,41 @@
-import setuptools
+from setuptools import find_packages
+from setuptools import setup
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import splitext
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="crowdstrike-falconpy",
-    version="0.1.7",
+    version="0.1.3",
     author="CrowdStrike",
     maintainer="Joshua Hiller",
     description="The CrowdStrike Falcon API SDK for Python 3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CrowdStrike/falconpy",
-    packages=["falconpy"],
+    packages=find_packages('falconpy'),
     package_dir={'': 'falconpy'},
+    py_modules=[splitext(basename(path))[0] for path in glob('falconpy/*.py')],
+    include_package_data=True,
     install_requires=[
-	"requests",
-	"urllib3"
+    	"requests",
+	    "urllib3"
     ],
     classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
