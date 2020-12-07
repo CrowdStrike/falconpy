3a37fc25e49c00e15bd33fcd9658f88f539563e5
jshcodes <74007258+jshcodes@users.noreply.github.com>
2020-12-07 11:58:44 -0500

    Packaging updates, Licensing fixes and minor README cleanup (#7)
    * Package layout updates
    
    * Package updates
    
    * Cleaning up tabs
    
    * README and LICENSE updates
    
    * More package testing

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-import setuptools
+from setuptools import find_packages
+from setuptools import setup
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import splitext
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="crowdstrike-falconpy",
     version="0.1.7",
     author="CrowdStrike",
@@ -12,19 +17,25 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CrowdStrike/falconpy",
-    packages=["falconpy"],
-    package_dir={'': 'falconpy'},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
     install_requires=[
-	"requests",
-	"urllib3"
+        "requests",
+        "urllib3"
     ],
     classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
