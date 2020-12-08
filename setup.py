6f675e9b8545bfb3fd5a7d5daace2b1a484ce25e
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-08 01:15:20 -0500

    Uber class fix for octet-stream file uploads

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="crowdstrike-falconpy",
-    version="0.1.8",
+    version="0.1.9",
     author="CrowdStrike",
     maintainer="Joshua Hiller",
     description="The CrowdStrike Falcon API SDK for Python 3",
