00b1208e4ed8d6d36ccc2e9500456700b55ba9fe
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-07 23:31:56 -0500

    v0.1.8 - Uber class custom headers

--- src/falconpy/__init__.py
+++ src/falconpy/__init__.py
@@ -4,7 +4,7 @@ falconpy
 The CrowdStrike Falcon API SDK
 """
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 __maintainer__ = 'Joshua Hiller'
 __author__ = 'CrowdStrike'
 __credits__ = 'CrowdStrike'
