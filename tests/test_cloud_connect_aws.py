284ac7c9f1118a8d54bf7cc4b9d9803f7855fcb5
Joshua Hiller <joshua.hiller@crowdstrike.com>
2020-12-28 12:09:25 -0500

    Additional unit tests

--- tests/test_cloud_connect_aws.py
+++ tests/test_cloud_connect_aws.py
@@ -16,8 +16,20 @@ from falconpy import cloud_connect_aws as FalconAWS
 auth = Authorization.TestAuthorization()
 auth.serviceAuth()
 falcon = FalconAWS.Cloud_Connect_AWS(access_token=auth.token)
-AllowedResponses = [200, 429] #Adding rate-limiting as an allowed response for now
-
+AllowedResponses = [200, 201, 429] #Adding rate-limiting as an allowed response for now
+accountPayload = {
+        "resources": [
+            {
+                # "cloudtrail_bucket_owner_id": cloudtrail_bucket_owner_id,
+                # "cloudtrail_bucket_region": cloudtrail_bucket_region,
+                # "external_id": external_id,
+                # "iam_role_arn": iam_role_arn,
+                # "id": local_account,
+                "rate_limit_reqs": 0,
+                "rate_limit_time": 0
+            }
+        ]
+    }
 class TestCloudConnectAWS:
     def serviceCCAWS_GetAWSSettings(self):
         if falcon.GetAWSSettings()["status_code"] in AllowedResponses:
@@ -26,7 +38,7 @@ class TestCloudConnectAWS:
             return False      
 
     def serviceCCAWS_QueryAWSAccounts(self):
-        if falcon.QueryAWSAccounts()["status_code"] in AllowedResponses:
+        if falcon.QueryAWSAccounts(parameters={"limit":1})["status_code"] in AllowedResponses:
             return True
         else:
             return False
@@ -34,6 +46,36 @@ class TestCloudConnectAWS:
     @pytest.mark.skipif(falcon.QueryAWSAccounts(parameters={"limit":1})["status_code"] == 429, reason="API rate limit reached")
     def serviceCCAWS_GetAWSAccounts(self):
         if falcon.GetAWSAccounts(ids=falcon.QueryAWSAccounts(parameters={"limit":1})["body"]["resources"][0]["id"])["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    @pytest.mark.skipif(falcon.QueryAWSAccounts(parameters={"limit":1})["status_code"] == 429, reason="API rate limit reached")
+    def serviceCCAWS_AccountUpdate(self):
+        account = falcon.QueryAWSAccounts(parameters={"limit":1})["body"]["resources"][0]
+        accountPayload["resources"][0]["cloudtrail_bucket_owner_id"] = account["cloudtrail_bucket_owner_id"]
+        accountPayload["resources"][0]["cloudtrail_bucket_region"] = account["cloudtrail_bucket_region"]
+        orig_external_id = account["external_id"]
+        accountPayload["resources"][0]["external_id"] = "UnitTesting"
+        accountPayload["resources"][0]["iam_role_arn"] = account["iam_role_arn"]
+        accountPayload["resources"][0]["id"] = account["id"]
+        if falcon.UpdateAWSAccounts(body=accountPayload)["status_code"] in AllowedResponses:
+            accountPayload["resources"][0]["external_id"] = orig_external_id
+            return True
+        else:
+            accountPayload["resources"][0]["external_id"] = orig_external_id
+            return False
+
+    @pytest.mark.skipif(falcon.QueryAWSAccounts(parameters={"limit":1})["status_code"] == 429, reason="API rate limit reached")
+    def serviceCCAWS_AccountDelete(self):
+        if falcon.DeleteAWSAccounts(ids=accountPayload["resources"][0]["id"])["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    @pytest.mark.skipif(falcon.QueryAWSAccounts(parameters={"limit":1})["status_code"] == 429, reason="API rate limit reached")
+    def serviceCCAWS_AccountRegister(self):
+        if falcon.ProvisionAWSAccounts(body=accountPayload)["status_code"] in AllowedResponses:
             return True
         else:
             return False
@@ -51,6 +93,31 @@ class TestCloudConnectAWS:
         else:
             return False
 
+    def serviceCCAWS_GenerateErrors(self):
+        # Garf the base_url so we force 500s for each method to cover all remaining code paths
+        falcon.base_url = "nowhere"
+        errorChecks = True
+        if falcon.QueryAWSAccounts()["status_code"] != 500:
+            errorChecks = False
+        if falcon.QueryAWSAccountsForIDs()["status_code"] != 500:
+            errorChecks = False
+        if falcon.GetAWSSettings()["status_code"] != 500:
+            errorChecks = False
+        if falcon.GetAWSAccounts(ids="1234567890")["status_code"] != 500:
+            errorChecks = False
+        if falcon.UpdateAWSAccounts(body={})["status_code"] != 500:
+            errorChecks = False
+        if falcon.DeleteAWSAccounts(ids="1234567890")["status_code"] != 500:
+            errorChecks = False
+        if falcon.ProvisionAWSAccounts(body={})["status_code"] != 500:
+            errorChecks = False
+        if falcon.CreateOrUpdateAWSSettings(body={})["status_code"] != 500:
+            errorChecks = False
+        if falcon.VerifyAWSAccountAccess(ids="1234567890", body={})["status_code"] != 500:
+            errorChecks = False
+
+        return errorChecks
+
     def test_GetAWSSettings(self):
         assert self.serviceCCAWS_GetAWSSettings() == True
 
@@ -62,9 +129,21 @@ class TestCloudConnectAWS:
 
     def test_VerifyAWSAccountAccess(self):
         assert self.serviceCCAWS_VerifyAWSAccountAccess() == True
-        
+
+    def test_AccountUpdate(self):
+        assert self.serviceCCAWS_AccountUpdate() == True
+    
+    def test_AccountDelete(self):
+        assert self.serviceCCAWS_AccountDelete() == True
+
     def test_QueryAWSAccountsForIDs(self):
         assert self.serviceCCAWS_QueryAWSAccountsForIDs() == True
 
-    def test_logout(self):
+    def test_AccountRegister(self):
+        assert self.serviceCCAWS_AccountRegister() == True
+
+    def test_Logout(self):
         assert auth.serviceRevoke() == True
+
+    def test_Errors(self):
+        assert self.serviceCCAWS_GenerateErrors() == True
---
+++ tests/test_uber_api_complete.py
@@ -0,0 +1,204 @@
+# test_uber_api_complete.py
+# This class tests the uber class
+
+import json
+import os
+import sys
+import pytest
+import datetime
+import hashlib
+#Import our sibling src folder into the path
+sys.path.append(os.path.abspath('src'))
+# Classes to test - manually imported from our sibling folder
+from falconpy import api_complete as FalconSDK
+
+AllowedResponses = [200, 400, 415, 429, 500] 
+
+if "DEBUG_API_ID" in os.environ and "DEBUG_API_SECRET" in os.environ:
+    config = {}
+    config["falcon_client_id"] = os.getenv("DEBUG_API_ID")
+    config["falcon_client_secret"] = os.getenv("DEBUG_API_SECRET")
+else:
+    cur_path = os.path.dirname(os.path.abspath(__file__))
+    if os.path.exists('%s/test.config' % cur_path):
+        with open('%s/test.config' % cur_path, 'r') as file_config:
+            config = json.loads(file_config.read())
+    else:
+        sys.exit(1)                
+
+
+falcon = FalconSDK.APIHarness(
+    creds={
+        "client_id": config["falcon_client_id"],
+        "client_secret": config["falcon_client_secret"]
+    }
+)
+falcon.authenticate()
+if not falcon.authenticated:
+    sys.exit(1)
+
+class TestUber:
+    def uberCCAWS_GetAWSSettings(self):
+        if falcon.command("GetAWSSettings")["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False 
+    
+    def uberCCAWS_QueryAWSAccounts(self):
+        if falcon.command("QueryAWSAccounts", parameters={"limit":1})["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False 
+
+    @pytest.mark.skipif(falcon.command("QueryAWSAccounts", parameters={"limit":1})["status_code"] == 429, reason="API rate limit reached")
+    def uberCCAWS_GetAWSAccounts(self):
+        if falcon.command("GetAWSAccounts", ids=falcon.command("QueryAWSAccounts", parameters={"limit":1})["body"]["resources"][0]["id"])["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    @pytest.mark.skipif(falcon.command("QueryAWSAccounts", parameters={"limit":1})["status_code"] == 429, reason="API rate limit reached")
+    def uberCCAWS_VerifyAWSAccountAccess(self):
+        if falcon.command("VerifyAWSAccountAccess", ids=falcon.command("QueryAWSAccounts", parameters={"limit":1})["body"]["resources"][0]["id"])["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_QueryAWSAccountsForIDs(self):
+        if falcon.command("QueryAWSAccountsForIDs", parameters={"limit":1})["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False 
+
+    def uberCCAWS_TestUploadDownload(self):
+        FILENAME="tests/testfile.png"
+        fmt='%Y-%m-%d %H:%M:%S'
+        stddate = datetime.datetime.now().strftime(fmt)
+        sdtdate = datetime.datetime.strptime(stddate, fmt)
+        sdtdate = sdtdate.timetuple()
+        jdate = sdtdate.tm_yday
+        jdate = "{}{}".format(stddate.replace("-","").replace(":","").replace(" ",""),jdate)
+        SOURCE="%s_source.png" % jdate
+        TARGET="tests/%s_target.png" % jdate
+        PAYLOAD = open(FILENAME, 'rb').read()
+        response = falcon.command('UploadSampleV3', file_name=SOURCE, data=PAYLOAD, content_type="application/octet-stream")
+        sha = response["body"]["resources"][0]["sha256"]
+        response = falcon.command("GetSampleV3", parameters={}, ids=sha)
+        open(TARGET, 'wb').write(response)
+        buf=65536
+        hash1 = hashlib.sha256()
+        with open(FILENAME, 'rb') as f:
+            while True:
+                data = f.read(buf)
+                if not data:
+                    break
+                hash1.update(data)
+        hash1 = hash1.hexdigest()
+        hash2 = hashlib.sha256()
+        with open(TARGET, 'rb') as f:
+            while True:
+                data = f.read(buf)
+                if not data:
+                    break
+                hash2.update(data)
+        hash2 = hash2.hexdigest()
+        if os.path.exists(TARGET):
+            os.remove(TARGET)
+        if hash1 == hash2:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_GenerateError(self):
+        if falcon.command("QueryAWSAccounts", partition=0)["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_GenerateInvalidPayload(self):
+        if falcon.command("refreshActiveStreamSession", partition=9, parameters={"action_name":"refresh_active_stream_session"})["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_OverrideAndHeader(self):
+        if falcon.command(action="", override="GET,/cloud-connect-aws/combined/accounts/v1", headers={"Nothing":"Special"})["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_BadCommand(self):
+        if falcon.command("IWantTheImpossible", parameters={"limit":1})["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_GenerateServerError(self):
+        if falcon.command("GetAWSAccounts", ids="123", data=['Kerash!'])["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_GenerateTokenError(self):
+        falcon.base_url = "'`\""
+        if falcon.deauthenticate() == False:
+            return True
+        else:
+            return False
+
+    def uberCCAWS_BadAuthentication(self):
+        falcon = FalconSDK.APIHarness(
+            creds={
+                "client_id": "BadClientID",
+                "client_secret": "BadClientSecret"
+            }
+        )
+        if falcon.command("QueryAWSAccounts", parameters={"limit":1})["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False 
+        
+
+    def test_GetAWSSettings(self):
+        assert self.uberCCAWS_GetAWSSettings() == True
+
+    def test_QueryAWSAccounts(self):
+        assert self.uberCCAWS_QueryAWSAccounts() == True
+
+    def test_GetAWSAccounts(self):
+        assert self.uberCCAWS_GetAWSAccounts() == True
+
+    def test_VerifyAWSAccountAccess(self):
+        assert self.uberCCAWS_VerifyAWSAccountAccess() == True
+    
+    def test_QueryAWSAccountsForIDs(self):
+        assert self.uberCCAWS_QueryAWSAccountsForIDs() == True
+
+    def test_UploadDownload(self):
+        assert self.uberCCAWS_TestUploadDownload() == True
+    
+    def test_GenerateError(self):
+        assert self.uberCCAWS_GenerateError() == True
+    
+    def test_GenerateInvalidPayload(self):
+        assert self.uberCCAWS_GenerateInvalidPayload() == True
+
+    def test_BadCommand(self):
+        assert self.uberCCAWS_BadCommand() == True
+
+    def test_OverrideAndHeader(self):
+        #Also check token auto-renewal
+        falcon.token_expiration=0
+        assert self.uberCCAWS_OverrideAndHeader() == True
+    
+    def test_GenerateServerError(self):
+        assert self.uberCCAWS_GenerateServerError() == True
+    
+    def test_logout(self):
+        assert falcon.deauthenticate() == True
+
+    def test_GenerateTokenError(self):
+        assert self.uberCCAWS_GenerateTokenError() == True
+
+    def test_BadAuthentication(self):
+        assert self.uberCCAWS_BadAuthentication() == True
