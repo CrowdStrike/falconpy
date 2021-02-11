208f0253095e392976b6f66353a0b5dc06b46ab9
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-02-11 15:56:58 -0500

    Version updated to 0.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="crowdstrike-falconpy",
-    version="0.2.3",
+    version="0.3.0",
     author="CrowdStrike",
     maintainer="Joshua Hiller",
     description="The CrowdStrike Falcon API SDK for Python 3",
