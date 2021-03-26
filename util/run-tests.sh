0901aa7f520912d51b9a66d8973c24c7ddace02a
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-03-26 16:47:36 -0400

    Local unit test adjustments

--- util/run-tests.sh
+++ util/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # At this point in time we are excluding FalconShell and FalconDebug from unit testing
 #
-coverage run --source=src/falconpy --omit=*shell*,*_debug.py -m pytest -s -v
+coverage run --source src/falconpy -m pytest -s -v
 coverage report
 bandit -r src
