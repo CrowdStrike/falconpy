a8261f6f3d4d16f5763206c0ad24b74d35bee88f
Caleb Schwartz <clbschwrtz15@gmail.com>
2021-03-31 11:14:06 -0700

    function stubs

--- src/falconpy/sensor_download.py
+++ src/falconpy/sensor_download.py
@@ -1,5 +1,20 @@
+from ._util import service_request, generate_error_result
+from ._service_class import ServiceClass
 
+class Sensor_Download(ServiceClass):
 
+    def GetCombinedSensorInstallersByQuery():
+        pass
 
-def thing():
-    pass
+    def DownloadSensorInstallerById(_id: str):
+        # _id is the sha256 of the sensor
+        pass
+
+    def GetSensorInstallersEntities():
+        pass
+
+    def GetSensorInstallersCCIDByQuery():
+        pass
+
+    def GetSensorInstallersByQuery():
+        pass
