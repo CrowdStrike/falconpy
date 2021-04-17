2b53ea43103dad65fa84414c6e188e2cc8c891d5
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-04-17 02:05:59 -0400

    Added MSSP Service Class (Falcon Flight Control)

---
+++ tests/test_mssp.py
@@ -0,0 +1,60 @@
+# test_mssp.py
+# This class tests the mssp service class
+import os
+import sys
+
+# Authentication via the test_authorization.py
+from tests import test_authorization as Authorization
+# Import our sibling src folder into the path
+sys.path.append(os.path.abspath('src'))
+# Classes to test - manually imported from sibling folder
+from falconpy.mssp import Flight_Control  # noqa: E402  pylint: disable=E0401
+
+auth = Authorization.TestAuthorization()
+auth.serviceAuth()
+falcon = Flight_Control(access_token=auth.token)
+AllowedResponses = [200, 429]  # Adding rate-limiting as an allowed response for now
+
+
+class TestFlightControl:
+    def serviceFlight_GenerateErrors(self):
+        falcon.base_url = "nowhere"
+        errorChecks = True
+        commandList = [
+            ["getChildren", "ids='12345678'"],
+            ["getCIDGroupMembersBy", "cid_group_ids='12345678'"],
+            ["addCIDGroupMembers", "body={}"],
+            ["deleteCIDGroupMembers", "body={}"],
+            ["getCIDGroupById", "cid_group_ids='12345678'"],
+            ["createCIDGroups", "body={}"],
+            ["deleteCIDGroups", "cid_group_ids='12345678'"],
+            ["updateCIDGroups", "body={}"],
+            ["getRolesByID", "ids='12345678'"],
+            ["addRole", "body={}"],
+            ["deleteRoles", "body={}"],
+            ["getUserGroupMembersByID", "user_group_ids='12345678'"],
+            ["getUserGroupsByID", "user_group_ids='12345678'"],
+            ["addUserGroupMembers", "body={}"],
+            ["deleteUserGroupMembers", "body={}"],
+            ["createUserGroup", "body={}"],
+            ["updateUserGroups", "body={}"],
+            ["deleteUserGroups", "body={}"],
+            ["queryChildren", ""],
+            ["queryCIDGroupMembers", ""],
+            ["queryCIDGroups", ""],
+            ["queryRoles", ""],
+            ["queryUserGroupMembers", ""],
+            ["queryUserGroups", ""]
+
+        ]
+        for cmd in commandList:
+            if eval("falcon.{}({})['status_code']".format(cmd[0], cmd[1])) != 500:
+                errorChecks = False
+
+        return errorChecks
+
+    def test_Logout(self):
+        assert auth.serviceRevoke() is True
+
+    def test_Errors(self):
+        assert self.serviceFlight_GenerateErrors() is True
