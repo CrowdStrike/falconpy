618976422ee6cd3ad520909252bf85cf0a1bfb91
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-04-19 06:33:49 -0400

    Unit test coverage parameters moved to stand-alone configuration file

--- util/run-tests.sh
+++ util/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # At this point in time we are excluding FalconShell and FalconDebug from unit testing
 #
-coverage run --source=src/falconpy --omit=src/falconpy/debug.py -m pytest -s -v
+coverage run --rcfile=util/coverage.config -m pytest -s -v
 coverage report
 bandit -r src
