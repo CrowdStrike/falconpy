d4e344828167089c85684e3b7a8e3adc85bd7a4c
Bryan McClellan <mccbryan.ops@gmail.com>
2021-06-05 18:08:26 -0500

    pepped and commented

--- samples/cspm_policies/get_cspm_policies.py
+++ samples/cspm_policies/get_cspm_policies.py
@@ -1,10 +1,10 @@
-#  ___  ____  ____     ___  ____  ____  _  _    ____   __   __    __  ___  __  ____  ____ 
+#  ___  ____  ____     ___  ____  ____  _  _    ____   __   __    __  ___  __  ____  ____
 # / __)(  __)(_  _)   / __)/ ___)(  _ \( \/ )  (  _ \ /  \ (  )  (  )/ __)(  )(  __)/ ___)
-#( (_ \ ) _)   )(    ( (__ \___ \ ) __// \/ \   ) __/(  O )/ (_/\ )(( (__  )(  ) _) \___ \
+# ( (_ \ ) _)   )(    ( (__ \___ \ ) __// \/ \   ) __/(  O )/ (_/\ )(( (__  )(  ) _) \___ \
 # \___/(____) (__)    \___)(____/(__)  \_)(_/  (__)   \__/ \____/(__)\___)(__)(____)(____/
 #
 # The below example uses the CSPM Registration Class
-# 
+#
 # Input parameters:
 #
 # --falcon_client_id or -f (client id of the API credentials with Horizon read capabilities)
@@ -20,18 +20,23 @@
 #
 #  python3 get_cspm_policies.py -f $FALCON_CLIENT_ID -s $FALCON_CLIENT_SECRET -c azure -o ~/Documents/azure-policies.csv
 #
-### 
+###
 
 import argparse
 import json
 import csv
 from falconpy import cspm_registration as FalconCSPM
 
-parser = argparse.ArgumentParser(description="Gather API client_id and client_secret from arguments")
-parser.add_argument("-f", "--falcon_client_id", help="Falcon Client ID", required=True)
-parser.add_argument("-s", "--falcon_client_secret", help="Falcon Client Secret", required=True)
-parser.add_argument("-o", "--output_file", help="Policy report output file", required=True)
-parser.add_argument("-c", "--cloud", help="Cloud provider (aws, azure, gcp)", required=False)
+parser = argparse.ArgumentParser(
+    description="Gather API client_id and client_secret from arguments")
+parser.add_argument("-f", "--falcon_client_id",
+                    help="Falcon Client ID", required=True)
+parser.add_argument("-s", "--falcon_client_secret",
+                    help="Falcon Client Secret", required=True)
+parser.add_argument("-o", "--output_file",
+                    help="Policy report output file", required=True)
+parser.add_argument(
+    "-c", "--cloud", help="Cloud provider (aws, azure, gcp)", required=False)
 args = parser.parse_args()
 
 falcon_client_id = args.falcon_client_id
@@ -40,12 +45,13 @@ data_file = args.output_file
 cloud = args.cloud
 
 falcon = FalconCSPM.CSPM_Registration(
-    creds={"client_id": falcon_client_id, 
-            "client_secret": falcon_client_secret
-})
+    creds={"client_id": falcon_client_id,
+           "client_secret": falcon_client_secret
+           })
 
 if cloud:
-    policies = falcon.GetCSPMPolicySettings("cloud-platform=" + cloud)['body']['resources']
+    policies = falcon.GetCSPMPolicySettings(
+        "cloud-platform=" + cloud)['body']['resources']
 else:
     policies = falcon.GetCSPMPolicySettings()['body']['resources']
 
@@ -61,20 +67,22 @@ for p in policies:
     for h in headers:
         try:
             if h == headers[-1]:
-                str_line = "\"{}\": \"{}\"".format(h,str(p[h]).strip("\n").replace('"',''))
+                str_line = "\"{}\": \"{}\"".format(
+                    h, str(p[h]).strip("\n").replace('"', ''))
             else:
-                str_line = "\"{}\": \"{}\", ".format(h,str(p[h]).strip("\n").replace('"',''))
+                str_line = "\"{}\": \"{}\", ".format(
+                    h, str(p[h]).strip("\n").replace('"', ''))
         except:
             if h == headers[-1]:
-                str_line = "\"{}\": \"{}\"".format(h,"")
+                str_line = "\"{}\": \"{}\"".format(h, "")
             else:
-                str_line = "\"{}\": \"{}\", ".format(h,"")
+                str_line = "\"{}\": \"{}\", ".format(h, "")
         policy += str_line
     new_dict = "{{{}}}".format(policy)
     list_dict.append(json.loads(new_dict))
 
 keys = list_dict[0].keys()
-with open(data_file, 'w', newline='')  as output_file:
+with open(data_file, 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(list_dict)
