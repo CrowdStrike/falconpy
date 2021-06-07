8b3298d861e6bc8574b9ce46c87d400bb1d221ec
Bryan McClellan <mccbryan.ops@gmail.com>
2021-06-07 08:05:29 -0500

    updated for config.json and function

--- samples/cspm_policies/get_cspm_policies.py
+++ samples/cspm_policies/get_cspm_policies.py
@@ -25,64 +25,98 @@
 import argparse
 import json
 import csv
+import os
+import sys
+import logging
 from falconpy import cspm_registration as FalconCSPM
 
+# Capture command line arguments
 parser = argparse.ArgumentParser(
     description="Gather API client_id and client_secret from arguments")
 parser.add_argument("-f", "--falcon_client_id",
-                    help="Falcon Client ID", required=True)
+                    help="Falcon Client ID", default=None, required=False)
 parser.add_argument("-s", "--falcon_client_secret",
-                    help="Falcon Client Secret", required=True)
+                    help="Falcon Client Secret", default=None, required=False)
 parser.add_argument("-o", "--output_file",
-                    help="Policy report output file", required=True)
+                    help="Policy report output file", required=False)
 parser.add_argument(
     "-c", "--cloud", help="Cloud provider (aws, azure, gcp)", required=False)
 args = parser.parse_args()
 
-falcon_client_id = args.falcon_client_id
-falcon_client_secret = args.falcon_client_secret
+# Grab our client_id and client_secret or exit
+config_file = '../config.json'
+if os.path.isfile(config_file):
+    with open(config_file, 'r') as file_config:
+        config = json.loads(file_config.read())
+        falcon_client_id = config['falcon_client_id']
+        falcon_client_secret = config['falcon_client_secret']
+elif args.falcon_client_id is not None and args.falcon_client_secret is not None:
+    falcon_client_id = args.falcon_client_id
+    falcon_client_secret = args.falcon_client_secret
+else:
+    logging.error(
+        " Please specify Falcon API Credentials with config.json or script arguments")
+    sys.exit()
+
 data_file = args.output_file
 cloud = args.cloud
 
+# Instantiate CSPM_Registration service class
 falcon = FalconCSPM.CSPM_Registration(
     creds={"client_id": falcon_client_id,
            "client_secret": falcon_client_secret
            })
 
+# Format api json data to accommodate for missing keys
+# The goal of this function is to bring uniformtity to the api
+# returned data so it can be reported in csv format
+
+
+def format_json_data(json_data):
+    length = 0
+    headers = []
+    for p in json_data:
+        if len(p.keys()) > length:
+            length = len(p.keys())
+            headers = [*p]
+    list_dict = []
+    for p in json_data:
+        policy = ""
+        for h in headers:
+            if h in p.keys():
+                if h == headers[-1]:
+                    str_line = "\"{}\": \"{}\"".format(
+                        h, str(p[h]).strip("\n").replace('"', ''))
+                else:
+                    str_line = "\"{}\": \"{}\", ".format(
+                        h, str(p[h]).strip("\n").replace('"', ''))
+            else:
+                if h == headers[-1]:
+                    str_line = "\"{}\": \"{}\"".format(h, "")
+                else:
+                    str_line = "\"{}\": \"{}\", ".format(h, "")
+            policy += str_line
+        new_dict = "{{{}}}".format(policy)
+        list_dict.append(json.loads(new_dict))
+    return list_dict
+
+
+# determine if we are reporting on a single cloud-platform
 if cloud:
     policies = falcon.GetCSPMPolicySettings(
         "cloud-platform=" + cloud)['body']['resources']
 else:
     policies = falcon.GetCSPMPolicySettings()['body']['resources']
 
-length = 0
-headers = []
-for p in policies:
-    if len(p.keys()) > length:
-        length = len(p.keys())
-        headers = [*p]
-list_dict = []
-for p in policies:
-    policy = ""
-    for h in headers:
-        try:
-            if h == headers[-1]:
-                str_line = "\"{}\": \"{}\"".format(
-                    h, str(p[h]).strip("\n").replace('"', ''))
-            else:
-                str_line = "\"{}\": \"{}\", ".format(
-                    h, str(p[h]).strip("\n").replace('"', ''))
-        except:
-            if h == headers[-1]:
-                str_line = "\"{}\": \"{}\"".format(h, "")
-            else:
-                str_line = "\"{}\": \"{}\", ".format(h, "")
-        policy += str_line
-    new_dict = "{{{}}}".format(policy)
-    list_dict.append(json.loads(new_dict))
+# Call format function on the returned api data
+return_data = format_json_data(policies)
 
-keys = list_dict[0].keys()
-with open(data_file, 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(list_dict)
+# Determine if an output file is specified and write out or print
+if data_file:
+    keys = return_data[0].keys()
+    with open(data_file, 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(return_data)
+else:
+    print(return_data)
