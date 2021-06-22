90a28d96cb9319aecc1de4135ec40b0b474e570b
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-06-22 00:42:22 -0400

    Added: Response Policies Service Class + Unit Test

---
+++ src/falconpy/response_policies.py
@@ -0,0 +1,235 @@
+"""
+ _______                        __ _______ __        __ __
+|   _   .----.-----.--.--.--.--|  |   _   |  |_.----|__|  |--.-----.
+|.  1___|   _|  _  |  |  |  |  _  |   1___|   _|   _|  |    <|  -__|
+|.  |___|__| |_____|________|_____|____   |____|__| |__|__|__|_____|
+|:  1   |                         |:  1   |
+|::.. . |   CROWDSTRIKE FALCON    |::.. . |    FalconPy
+`-------'                         `-------'
+
+OAuth2 API - Customer SDK
+
+response_policies - CrowdStrike Falcon Real Time Response Policies API interface class
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+"""
+from ._util import service_request, force_default, args_to_params
+from ._service_class import ServiceClass
+from ._endpoint._response_policies import _response_policies_endpoints as Endpoints
+
+
+class Response_Policies(ServiceClass):
+    """ The only requirement to instantiate an instance of this class
+        is a valid token provided by the Falcon API SDK OAuth2 class.
+    """
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def queryCombinedRTResponsePolicyMembers(self: object, parameters: dict = None, **kwargs) -> dict:
+        """Search for members of a Response policy in your environment by providing an FQL filter and paging details.
+           Returns a set of host details which match the filter criteria
+        """
+        # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/queryCombinedRTResponsePolicyMembers
+        operation_id = "queryCombinedRTResponsePolicyMembers"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        returned = service_request(caller=self,
+                                   method="GET",
+                                   endpoint=target_url,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def queryCombinedRTResponsePolicies(self: object, parameters: dict = None, **kwargs) -> dict:
+        """Search for Response Policies in your environment by providing an FQL filter and paging details.
+           Returns a set of Response Policies which match the filter criteria
+        """
+        # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/queryCombinedRTResponsePolicies
+        operation_id = "queryCombinedRTResponsePolicies"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        returned = service_request(caller=self,
+                                   method="GET",
+                                   endpoint=target_url,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def performRTResponsePoliciesAction(self: object, body: dict, parameters: dict = None, **kwargs) -> dict:
+        """Perform the specified action on the Response Policies specified in the request"""
+        # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/performRTResponsePoliciesAction
+        operation_id = "performRTResponsePoliciesAction"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        body_payload = body
+        returned = service_request(caller=self,
+                                   method="POST",
+                                   endpoint=target_url,
+                                   body=body_payload,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    def setRTResponsePoliciesPrecedence(self: object, body: dict) -> dict:
+        """Sets the precedence of Response Policies based on the order of IDs specified in the request.
+           The first ID specified will have the highest precedence and the last ID specified will have the lowest.
+           You must specify all non-Default Policies for a platform when updating precedence
+        """
+        # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/setRTResponsePoliciesPrecedence
+        operation_id = "setRTResponsePoliciesPrecedence"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        body_payload = body
+        returned = service_request(caller=self,
+                                   method="POST",
+                                   endpoint=target_url,
+                                   body=body_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def getRTResponsePolicies(self: object, parameters: dict = None, **kwargs) -> dict:
+        """Retrieve a set of Response Policies by specifying their IDs"""
+        # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/response-policies/getRTResponsePolicies
+        operation_id = "getRTResponsePolicies"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}".replace("?ids={}", "")
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        returned = service_request(caller=self,
+                                   method="GET",
+                                   endpoint=target_url,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    def createRTResponsePolicies(self: object, body: dict) -> dict:
+        """Create Response Policies by specifying details about the policy to create"""
+        # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/createRTResponsePolicies
+        operation_id = "createRTResponsePolicies"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        body_payload = body
+        returned = service_request(caller=self,
+                                   method="POST",
+                                   endpoint=target_url,
+                                   body=body_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def deleteRTResponsePolicies(self: object, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
+        """Delete a set of Response Policies by specifying their IDs"""
+        # [DELETE] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #              /response-policies/deleteRTResponsePolicies
+        operation_id = "deleteRTResponsePolicies"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}".replace("?ids={}", "")
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        returned = service_request(caller=self,
+                                   method="DELETE",
+                                   endpoint=target_url,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    def updateRTResponsePolicies(self: object, body: dict) -> dict:  # pylint: disable=C0103  # Matching API
+        """Update Response Policies by specifying the ID of the policy and details to update"""
+        # [PATCH] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/updateRTResponsePolicies
+        operation_id = "updateRTResponsePolicies"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        body_payload = body
+        returned = service_request(caller=self,
+                                   method="PATCH",
+                                   endpoint=target_url,
+                                   body=body_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def queryRTResponsePolicyMembers(self: object, parameters: dict = None, **kwargs) -> dict:
+        """Search for members of a Response policy in your environment by providing an FQL filter and paging details.
+           Returns a set of Agent IDs which match the filter criteria.
+        """
+        # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/queryRTResponsePolicyMembers
+        operation_id = "queryRTResponsePolicyMembers"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        returned = service_request(caller=self,
+                                   method="GET",
+                                   endpoint=target_url,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
+
+    @force_default(defaults=["parameters"], default_types=["dict"])
+    def queryRTResponsePolicies(self: object, parameters: dict = None, **kwargs) -> dict:
+        """Search for Response Policies in your environment by providing an FQL filter with sort and/or paging details.
+           This returns a set of Response Policy IDs that match the given criteria.
+        """
+        # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
+        #           /response-policies/queryRTResponsePolicies
+        operation_id = "queryRTResponsePolicies"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
+        returned = service_request(caller=self,
+                                   method="GET",
+                                   endpoint=target_url,
+                                   params=parameter_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
---
+++ tests/test_response_policies.py
@@ -0,0 +1,42 @@
+# test_response_policies.py
+# This class tests the Response_Policies service class
+import os
+import sys
+
+# Authentication via the test_authorization.py
+from tests import test_authorization as Authorization
+# Import our sibling src folder into the path
+sys.path.append(os.path.abspath('src'))
+# Classes to test - manually imported from sibling folder
+from falconpy.response_policies import Response_Policies as FalconRTRPolicy
+
+auth = Authorization.TestAuthorization()
+auth.serviceAuth()
+falcon = FalconRTRPolicy(access_token=auth.token)
+AllowedResponses = [200, 201, 400, 404, 429]
+
+
+class TestRTRPolicy:
+    def serviceRTRPolicy_RunAllTests(self):
+        errorChecks = True
+        commandList = [
+            ["queryCombinedRTResponsePolicyMembers", "limit=1"],
+            ["queryCombinedRTResponsePolicies", "limit=1,id=12345678"],
+            ["performRTResponsePoliciesAction", "action_name='enable',body={'ids':['12345678']}"],
+            ["setRTResponsePoliciesPrecedence", "body={'ids':['12345678','98765432'],'platform_name':'Windows'}"],
+            ["getRTResponsePolicies", "ids='01234567890123456789012345678901'"],
+            ["createRTResponsePolicies", "body={'resources': [{'settings': [{'id': '12345678'}]}]}"],  # Generates a 400
+            ["deleteRTResponsePolicies", "ids='01234567890123456789012345678901'"],
+            ["updateRTResponsePolicies", "body={}"],  # Generates a 400
+            ["queryRTResponsePolicyMembers", "parameters={'limit':1,'ids':['12345678']}"],
+            ["queryRTResponsePolicies", "limit=1"],
+        ]
+        for cmd in commandList:
+            result = eval("falcon.{}({})".format(cmd[0], cmd[1]))
+            if result['status_code'] not in AllowedResponses:
+                errorChecks = False
+
+        return errorChecks
+
+    def test_RunAllTests(self):
+        assert self.serviceRTRPolicy_RunAllTests() is True
