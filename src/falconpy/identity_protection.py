1c24f1e9addcdbb26d61ac770a1904024614e82c
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-07-28 17:52:06 -0400

    Added initial IDP Service Class

---
+++ src/falconpy/identity_protection.py
@@ -0,0 +1,69 @@
+"""
+ _______                        __ _______ __        __ __
+|   _   .----.-----.--.--.--.--|  |   _   |  |_.----|__|  |--.-----.
+|.  1___|   _|  _  |  |  |  |  _  |   1___|   _|   _|  |    <|  -__|
+|.  |___|__| |_____|________|_____|____   |____|__| |__|__|__|_____|
+|:  1   |                         |:  1   |
+|::.. . |   CROWDSTRIKE FALCON    |::.. . |    FalconPy
+`-------'                         `-------'
+
+OAuth2 API - Customer SDK
+
+identity_protection - Identity Protection API Interface Class
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+"""
+# pylint: disable=C0103  # Aligning method names to API operation IDs
+from ._util import service_request
+from ._service_class import ServiceClass
+from ._endpoint._identity_protection import _identity_protection_endpoints as Endpoints
+
+
+class Identity_Protection(ServiceClass):
+    """The only requirement to instantiate an instance of this class
+       is a valid token provided by the Falcon API SDK OAuth2 class, a
+       existing instance of the authentication class as an object or a
+       valid set of credentials.
+    """
+
+    def GraphQL(self: object, body: dict) -> dict:
+        """Identity Protection GraphQL API. Allows to retrieve entities, timeline activities,
+           identity-based incidents and security assessment. Allows to perform actions on entities
+           and identity-based incidents."""
+        # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html
+        #           /identity-protection/api.preempt.proxy.post.graphql
+        operation_id = "api_preempt_proxy_post_graphql"
+        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
+        header_payload = self.headers
+        body_payload = body
+        returned = service_request(caller=self,
+                                   method="POST",
+                                   endpoint=target_url,
+                                   body=body_payload,
+                                   headers=header_payload,
+                                   verify=self.ssl_verify
+                                   )
+        return returned
