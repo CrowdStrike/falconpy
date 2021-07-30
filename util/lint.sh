265537323e7d4fde0994abe21048eca2b3ac3861
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-07-29 22:34:49 -0400

    Added pylint to the developer lint utility script

--- util/lint.sh
+++ util/lint.sh
@@ -3,6 +3,7 @@ if ! [[ -z "$1" ]]
 then
 	TARGET=$1
 else
-	TARGET="."
+	TARGET="src/falconpy"
 fi
 flake8 $TARGET --count --exit-zero --max-complexity=15 --max-line-length=127 --statistics
+pylint $TARGET --exit-zero --max-line-length=127 --disable=R0801
