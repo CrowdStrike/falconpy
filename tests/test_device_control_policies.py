33507dc7dec2b5b28e37412d3081508783070475
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-01 14:23:35 -0400

    Linting

--- tests/test_device_control_policies.py
+++ tests/test_device_control_policies.py
@@ -27,13 +27,17 @@ class TestDeviceControlPolicy:
             return False
 
     def serviceDeviceControlPolicies_queryDeviceControlPolicyMembers(self):
-        if falcon.queryDeviceControlPolicyMembers(parameters={"id": falcon.queryDeviceControlPolicies(parameters={"limit": 1})["body"]["resources"][0]})["status_code"] in AllowedResponses:
+        if falcon.queryDeviceControlPolicyMembers(
+                parameters={"id": falcon.queryDeviceControlPolicies(parameters={"limit": 1})["body"]["resources"][0]}
+                )["status_code"] in AllowedResponses:
             return True
         else:
             return False
 
     def serviceDeviceControlPolicies_getDeviceControlPolicies(self):
-        if falcon.getDeviceControlPolicies(ids=falcon.queryDeviceControlPolicies(parameters={"limit": 1})["body"]["resources"][0])["status_code"] in AllowedResponses:
+        if falcon.getDeviceControlPolicies(
+                ids=falcon.queryDeviceControlPolicies(parameters={"limit": 1})["body"]["resources"][0]
+                )["status_code"] in AllowedResponses:
             return True
         else:
             return False
@@ -45,7 +49,11 @@ class TestDeviceControlPolicy:
             return False
 
     def serviceDeviceControlPolicies_queryCombinedDeviceControlPolicyMembers(self):
-        if falcon.queryCombinedDeviceControlPolicyMembers(parameters={"id": falcon.queryCombinedDeviceControlPolicies(parameters={"limit": 1})["body"]["resources"][0]["id"]})["status_code"] in AllowedResponses:
+        if falcon.queryCombinedDeviceControlPolicyMembers(
+                parameters={"id": falcon.queryCombinedDeviceControlPolicies(
+                    parameters={"limit": 1}
+                    )["body"]["resources"][0]["id"]}
+                )["status_code"] in AllowedResponses:
             return True
         else:
             return False
@@ -73,25 +81,33 @@ class TestDeviceControlPolicy:
         return errorChecks
 
     def test_queryDeviceControlPolicies(self):
-        assert self.serviceDeviceControlPolicies_queryDeviceControlPolicies() == True
+        assert self.serviceDeviceControlPolicies_queryDeviceControlPolicies() is True
 
-    @pytest.mark.skipif(falcon.queryDeviceControlPolicies(parameters={"limit": 1})["status_code"] == 429, reason="API rate limit reached")
+    @pytest.mark.skipif(
+        falcon.queryDeviceControlPolicies(parameters={"limit": 1})["status_code"] == 429, reason="API rate limit reached"
+        )
     def test_queryDeviceControlPolicyMembers(self):
-        assert self.serviceDeviceControlPolicies_queryDeviceControlPolicyMembers() == True
+        assert self.serviceDeviceControlPolicies_queryDeviceControlPolicyMembers() is True
 
-    @pytest.mark.skipif(falcon.queryDeviceControlPolicies(parameters={"limit": 1})["status_code"] == 429, reason="API rate limit reached")    
+    @pytest.mark.skipif(
+        falcon.queryDeviceControlPolicies(parameters={"limit": 1})["status_code"] == 429, reason="API rate limit reached"
+        )
     def test_getDeviceControlPolicies(self):
-        assert self.serviceDeviceControlPolicies_getDeviceControlPolicies() == True
+        assert self.serviceDeviceControlPolicies_getDeviceControlPolicies() is True
 
     def test_queryCombinedDeviceControlPolicies(self):
-        assert self.serviceDeviceControlPolicies_queryCombinedDeviceControlPolicies() == True
+        assert self.serviceDeviceControlPolicies_queryCombinedDeviceControlPolicies() is True
 
-    @pytest.mark.skipif(falcon.queryCombinedDeviceControlPolicies(parameters={"limit": 1})["status_code"] == 429, reason="API rate limit reached")
+    @pytest.mark.skipif(
+        falcon.queryCombinedDeviceControlPolicies(
+            parameters={"limit": 1}
+            )["status_code"] == 429, reason="API rate limit reached"
+        )
     def test_queryCombinedDeviceControlPolicyMembers(self):
-        assert self.serviceDeviceControlPolicies_queryCombinedDeviceControlPolicyMembers() == True
+        assert self.serviceDeviceControlPolicies_queryCombinedDeviceControlPolicyMembers() is True
 
     def test_Logout(self):
-        assert auth.serviceRevoke() == True
+        assert auth.serviceRevoke() is True
 
     def test_Errors(self):
-        assert self.serviceDeviceControlPolicies_GenerateErrors() == True
+        assert self.serviceDeviceControlPolicies_GenerateErrors() is True
