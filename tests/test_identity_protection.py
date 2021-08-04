d21a851bcbb3ac19a7309aa6b99363dfae3d72d0
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-03 23:19:07 -0400

    Adjusted IDP unit test to decode responses

--- tests/test_identity_protection.py
+++ tests/test_identity_protection.py
@@ -2,35 +2,30 @@
 # This class tests the identity_protection service class
 import os
 import sys
-
+import json
 # Authentication via the test_authorization.py
 from tests import test_authorization as Authorization
 # Import our sibling src folder into the path
 sys.path.append(os.path.abspath('src'))
 # Classes to test - manually imported from sibling folder
+# flake8: noqa=E402
 from falconpy.identity_protection import Identity_Protection as FalconIDP
 
 auth = Authorization.TestAuthorization()
 auth.serviceAuth()
-falcon = FalconIDP(access_token=auth.token)
+
+falcon = FalconIDP(creds=json.loads(str(auth.config).replace("falcon_","").replace("'",'"')))
 AllowedResponses = [200, 429]  # Adding rate-limiting as an allowed response for now
 
 
 class TestIdentityProtection:
-    def serviceIDP_GenerateErrors(self):
-        falcon.base_url = "nowhere"
-        errorChecks = True
-        commandList = [
-            ["GraphQL", "body={}"]
-        ]
-        for cmd in commandList:
-            if eval("falcon.{}({})['status_code']".format(cmd[0], cmd[1])) not in [400, 403, 500]:
-                errorChecks = False
+    def serviceIDP_GraphQL(self):
+        payload = {"query":"{\n  entities(first: 1)\n  {\n    nodes {\n      entityId    \n    }\n  }\n}"}
+        # GraphQL is currently returning results as binary encoded
+        if json.loads(falcon.GraphQL(body=payload).decode())["extensions"]["remainingPoints"] > 0:
+            return True
+        else:
+            return False
 
-        return errorChecks
-
-    def test_Logout(self):
-        assert auth.serviceRevoke() is True
-
-    def test_Errors(self):
-        assert self.serviceIDP_GenerateErrors() is True
+    def test_GraphQL(self):
+        assert self.serviceIDP_GraphQL() is True
