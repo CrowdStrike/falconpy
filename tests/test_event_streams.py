96325d3c9f7628528c4f729e995ae607cd59ebba
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-08 23:53:19 -0400

    Refactored. Fixed refresh test.

--- tests/test_event_streams.py
+++ tests/test_event_streams.py
@@ -1,7 +1,5 @@
 # test_event_streams.py
 # This class tests the event_streams service class
-
-import json
 import os
 import sys
 import datetime
@@ -10,35 +8,50 @@ import pytest
 # Authentication via the test_authorization.py
 from tests import test_authorization as Authorization
 
-#Import our sibling src folder into the path
+# Import our sibling src folder into the path
 sys.path.append(os.path.abspath('src'))
 # Classes to test - manually imported from sibling folder
-from falconpy import event_streams as FalconStream
+from falconpy.event_streams import Event_Streams
 
 auth = Authorization.TestAuthorization()
-auth.serviceAuth()
-falcon = FalconStream.Event_Streams(access_token=auth.token)
-AllowedResponses = [200, 429] #Adding rate-limiting as an allowed response for now
+# Moving to credential authentication
+auth.getConfig()
+falcon = Event_Streams(creds={"client_id": auth.config["falcon_client_id"],
+                              "client_secret": auth.config["falcon_client_secret"]})
+
+AllowedResponses = [200, 429]  # Adding rate-limiting as an allowed response for now
 appId = "pytest-event_streams-unit-test"
+
+
 class TestEventStreams:
 
     def serviceStream_listAvailableStreamsOAuth2(self):
-        if falcon.listAvailableStreamsOAuth2(parameters={"appId":"pytest-event_streams-unit-test"})["status_code"] in AllowedResponses:
+        if falcon.listAvailableStreamsOAuth2(
+                parameters={"appId": f"{appId}"}
+                )["status_code"] in AllowedResponses:
             return True
         else:
             return False
-          
+
     def serviceStream_refreshActiveStreamSession(self):
-        avail = falcon.listAvailableStreamsOAuth2(parameters={"appId":"pytest-event_streams-unit-test"})
+        avail = falcon.listAvailableStreamsOAuth2(parameters={"appId": f"{appId}"})
         t1 = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S +0000')
-        headers = {'Authorization': 'Token %s' % (avail["body"]["resources"][0]["sessionToken"]["token"]), 'Date': t1, 'Connection': 'Keep-Alive'}
+        headers = {
+            'Authorization': 'Token %s' % (
+                avail["body"]["resources"][0]["sessionToken"]["token"]
+                ), 'Date': t1, 'Connection': 'Keep-Alive'
+            }
         stream = requests.get(avail["body"]["resources"][0]["dataFeedURL"], headers=headers, stream=True)
         with stream:
-            if falcon.refreshActiveStreamSession(parameters={"appId": "pytest-event_streams-unit-test", "action_name":"refresh_active_stream_session"}, partition=0)["status_code"] in AllowedResponses:
+            result = falcon.refreshActiveStreamSession(appId=f"{appId}",
+                                                       action_name="refresh_active_stream_session",
+                                                       partition="0"
+                                                       )
+            if result["status_code"] in AllowedResponses:
                 return True
             else:
                 return False
-    
+
     def serviceStream_GenerateErrors(self):
         falcon.base_url = "nowhere"
         errorChecks = True
@@ -49,15 +62,25 @@ class TestEventStreams:
 
         return errorChecks
 
-    def test_listAvailableStreamsOAuth2(self):
-        assert self.serviceStream_listAvailableStreamsOAuth2() == True
+    def serviceStream_Logout(self):
+        if falcon.auth_object.revoke(falcon.auth_object.token()["body"]["access_token"])["status_code"] == 200:
+            return True
+        else:
+            return False
 
-    #@pytest.mark.skipif(falcon.listAvailableStreamsOAuth2(parameters={"appId":"pytest-event_streams-unit-test"})["status_code"] == 429, reason="API rate limit reached")
-    # def test_refreshActiveStreamSession(self):
-    #     assert self.serviceStream_refreshActiveStreamSession() == True
+    def test_list(self):
+        assert self.serviceStream_listAvailableStreamsOAuth2() is True
 
-    def test_Logout(self):
-        assert auth.serviceRevoke() == True
+    @pytest.mark.skipif(
+      falcon.listAvailableStreamsOAuth2(
+          parameters={"appId": "pytest-event_streams-unit-test"}
+          )["status_code"] == 429, reason="API rate limit reached"
+      )
+    def test_refresh(self):
+        assert self.serviceStream_refreshActiveStreamSession() is True
 
-    def test_Errors(self):
-        assert self.serviceStream_GenerateErrors() == True
+    def test_logout(self):
+        assert self.serviceStream_Logout() is True
+
+    def test_errors(self):
+        assert self.serviceStream_GenerateErrors() is True
