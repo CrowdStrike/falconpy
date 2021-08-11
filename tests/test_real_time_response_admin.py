d9965ff2fcf524fa45b812bc44531bd775794896
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-11 00:57:58 -0400

    Expanded unit testing to better cover file mgmt.

--- tests/test_real_time_response_admin.py
+++ tests/test_real_time_response_admin.py
@@ -4,6 +4,7 @@ test_real_time_response_admin.py - This class tests the real_time_response_admin
 import datetime
 import os
 import sys
+import json
 # Authentication via the test_authorization.py
 from tests import test_authorization as Authorization
 # Import our sibling src folder into the path
@@ -16,13 +17,44 @@ auth.getConfig()
 falcon = FalconRTR(creds={"client_id": auth.config["falcon_client_id"],
                           "client_secret": auth.config["falcon_client_secret"]
                           })
-AllowedResponses = [200, 201, 400, 404, 429]
+AllowedResponses = [200, 201, 202, 400, 404, 429]
 
 
 class TestRTR:
     """RTR Admin test harness"""
     @staticmethod
-    def rtra_test_all_code_paths():
+    def rtra_retrieve_script_id(script_name: str):
+        """Retrieve a script ID by name"""
+        found_id = "1234567890"  # Force an error if we can't find it
+        scripts = falcon.list_scripts()
+        for resource in scripts["body"]["resources"]:
+            script = falcon.get_scripts(ids=resource)
+            for file in script["body"]["resources"]:
+                if "name" in file:
+                    if file["name"] == script_name:
+                        found_id = file["id"]
+
+        return found_id
+
+    @staticmethod
+    def rtra_retrieve_file_id(file_name: str):
+        """Retrieve a script ID by name"""
+        found_id = "1234567890"  # Force an error if we can't find it
+        files = falcon.list_put_files()
+        for resource in files["body"]["resources"]:
+            file = falcon.get_put_files(ids=resource)
+            for item in file["body"]["resources"]:
+                if "name" in item:
+                    if item["name"] == file_name:
+                        found_id = item["id"]
+
+        return found_id
+
+    def rtra_create_updated_payload(self, file_name: str, orig_payload: dict):
+        orig_payload["id"] = self.rtra_retrieve_script_id(file_name)
+        return orig_payload
+
+    def rtra_test_all_code_paths(self):
         """Tests all code paths, accepts all errors except 500"""
         upload_file = "tests/testfile.png"
         fmt = '%Y-%m-%d %H:%M:%S'
@@ -32,30 +64,45 @@ class TestRTR:
         jdate = sdtdate.tm_yday
         jdate = "{}{}".format(stddate.replace("-", "").replace(":", "").replace(" ", ""), jdate)
         upload_filename = "%s_testfile.png" % jdate
+        script_filename = "%s_testscript" % jdate
 
         file_payload = {'name': upload_filename, 'description': 'FalconPy Unit Testing'}
         files_detail = [
             ('file', ('testfile.png', open(upload_file, 'rb'), 'image/png'))
         ]
-
+        script_payload = {
+            'name': script_filename,
+            'description': 'FalconPy Unit Testing',
+            'permission_type': 'private',
+            'content': 'Write-Output "This is a processing script."'
+        }
+        new_script_payload = json.loads(json.dumps(script_payload))
+        new_script_payload["content"] = 'Write-Output "This is an updated processing script."'
+        script_detail = [('file', (f"{script_filename}", 'application/script'))]
         error_checks = True
         tests = {
-            "batch_admin_cmd": falcon.BatchAdminCmd(body={})["status_code"],
-            "check_admin_command_status": falcon.RTR_CheckAdminCommandStatus(parameters={})["status_code"],
-            "execute_admin_command": falcon.RTR_ExecuteAdminCommand(body={})["status_code"],
-            "get_put_files": falcon.RTR_GetPut_Files(ids='12345678')["status_code"],
+            "batch_admin_cmd": falcon.BatchAdminCmd(body={})["status_code"],                                    # 400
+            "check_admin_command_status": falcon.RTR_CheckAdminCommandStatus(parameters={})["status_code"],     # 400
+            "execute_admin_command": falcon.RTR_ExecuteAdminCommand(body={})["status_code"],                    # 400
             "create_put_files": falcon.RTR_CreatePut_Files(data=file_payload, files=files_detail)["status_code"],
-            "delete_put_files": falcon.RTR_DeletePut_Files(ids='12345678')["status_code"],
-            "get_scripts": falcon.RTR_GetScripts(ids='12345678')["status_code"],
-            "create_scripts": falcon.RTR_CreateScripts(data={}, files=[])["status_code"],
-            "delete_scripts": falcon.RTR_DeleteScripts(ids='12345678')["status_code"],
-            "update_scripts": falcon.RTR_UpdateScripts(data={}, files=[])["status_code"],
+            "get_put_files": falcon.RTR_GetPut_Files(ids=self.rtra_retrieve_file_id(file_name=upload_filename))["status_code"],
+            "delete_put_files": falcon.RTR_DeletePut_Files(
+                ids=self.rtra_retrieve_file_id(file_name=upload_filename)
+                )["status_code"],
+            "create_scripts": falcon.RTR_CreateScripts(data=script_payload, files=script_detail)["status_code"],
+            "get_scripts": falcon.RTR_GetScripts(ids=self.rtra_retrieve_script_id(script_filename))["status_code"],
+            "update_scripts": falcon.RTR_UpdateScripts(
+                data=self.rtra_create_updated_payload(script_filename, new_script_payload), files=script_detail
+                )["status_code"],
+            "delete_scripts": falcon.RTR_DeleteScripts(ids=self.rtra_retrieve_script_id(script_filename))["status_code"],
             "list_put_files": falcon.RTR_ListPut_Files()["status_code"],
             "list_scripts": falcon.RTR_ListScripts()["status_code"]
         }
         for key in tests:
             if tests[key] not in AllowedResponses:
                 error_checks = False
+
+            # print(f"{key} processed with a {tests[key]} response")
 
         return error_checks
 
