87f16f2a6eca874f77dc8fc22961778d7af867d5
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-12 04:02:26 -0400

    Fix typo

--- src/falconpy/_util.py
+++ src/falconpy/_util.py
@@ -141,7 +141,7 @@ def force_default(defaults: list, default_types: list = None):
                     kwargs[element] = get_default(default_types, element_count)
                 # Increment our tracker for our sibling default_types list
                 element_count += 1
-            # The passed us an argument but did not specify what it was (non-keyword) [Issue #263]
+            # They passed us an argument but did not specify what it was (non-keyword) [Issue #263]
             if len(args) > 1:
                 args = [args[0]]
             return func(*args, **kwargs)
