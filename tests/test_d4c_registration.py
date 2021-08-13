a0e0d749efc34b89d4655f68738eea1c1eef94b5
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-13 16:14:42 -0400

    Refactored to the latest unit test pattern.

--- tests/test_d4c_registration.py
+++ tests/test_d4c_registration.py
@@ -1,8 +1,8 @@
-# test_d4c_registration.py
-# This class tests the Discover for Cloud registration service class
+"""
+test_d4c_registration.py - This class tests the Discover for Cloud registration service class
+"""
 import os
 import sys
-
 # Authentication via test_authorization.py
 from tests import test_authorization as Authorization
 # Import our sibling src folder into the path
@@ -19,7 +19,13 @@ AllowedResponses = [200, 429, 404]
 
 
 class TestD4CRegistration:
-    def serviceD4C_GetCSPMAzureUserScriptsAttachment(self):
+    """
+    Test harness for the D4C Registration Service Class
+    """
+    def d4c_get_azure_user_scripts_attachment(self):
+        """
+        get_azure_user_scripts_attachment
+        """
         returned = False
         result = falcon.GetCSPMAzureUserScriptsAttachment()
         if isinstance(result, (bytes)):
@@ -31,7 +37,10 @@ class TestD4CRegistration:
 
         return returned
 
-    def serviceD4C_GetCSPMAzureUserScripts(self):
+    def d4c_get_azure_user_scripts(self):
+        """
+        get_azure_user_scripts
+        """
         returned = False
         result = falcon.GetCSPMAzureUserScripts()
         if isinstance(result, (bytes)):
@@ -43,7 +52,10 @@ class TestD4CRegistration:
 
         return returned
 
-    def serviceD4C_GetCSPMGCPUserScriptsAttachment(self):
+    def d4c_get_gcp_user_scripts_attachment(self):
+        """
+        get_gcp_user_scripts_attachment
+        """
         returned = False
         result = falcon.GetCSPMGCPUserScriptsAttachment()
         if isinstance(result, (bytes)):
@@ -55,7 +67,10 @@ class TestD4CRegistration:
 
         return returned
 
-    def serviceD4C_GetCSPMGCPUserScripts(self):
+    def d4c_get_gcp_user_scripts(self):
+        """
+        get_gcp_user_scripts
+        """
         returned = False
         result = falcon.GetCSPMGCPUserScripts()
         if isinstance(result, (bytes)):
@@ -67,34 +82,63 @@ class TestD4CRegistration:
 
         return returned
 
-    def serviceD4C_GenerateErrors(self):
+    def d4c_generate_errors(self):
+        """
+        Test every code path within every method by generating 500s, does not hit the API
+        """
         falcon.base_url = "nowhere"
-        errorChecks = True
-        commandList = [
-            ["GetCSPMAzureAccount", "ids='12345678', scan_type='dry'"],
-            ["UpdateCSPMAzureAccountClientID", "ids='12345678'"],
-            ["GetCSPMCGPAccount", "ids='12345678', parameters={'scan_type': 'dry'}"],
-            ["GetCSPMGCPAccount", "ids='12345678'"],  # Test the typo fix version
-            ["CreateCSPMGCPAccount", "body={}"],
-            ["CreateCSPMAzureAccount", "body={}"]
-        ]
-        for cmd in commandList:
-            if eval("falcon.{}({})['status_code']".format(cmd[0], cmd[1])) != 500:
-                errorChecks = False
+        error_checks = True
+        tests = {
+            "get_azure_account": falcon.GetCSPMAzureAccount(ids='12345678', scan_type='dry')["status_code"],
+            "update_azure_account_client_id": falcon.UpdateCSPMAzureAccountClientID(ids='12345678')["status_code"],
+            "get_cgp_account": falcon.GetCSPMCGPAccount(ids='12345678', parameters={'scan_type': 'dry'})["status_code"],
+            "get_gcp_acocunt": falcon.GetCSPMGCPAccount(ids='12345678')["status_code"],  # Test the typo fix version
+            "create_gcp_account": falcon.CreateCSPMGCPAccount(body={})["status_code"],
+            "create_azure_account": falcon.CreateCSPMAzureAccount(body={})["status_code"]
+        }
+        for key in tests:
+            if tests[key] != 500:
+                error_checks = False
 
-        return errorChecks
+            # print(f"{key} processed with a {tests[key]} response")
+
+        return error_checks
 
     def test_GetCSPMAzureUserScriptsAttachment(self):
-        assert self.serviceD4C_GetCSPMAzureUserScriptsAttachment() is True
+        """
+        Pytest harness hook
+        """
+        assert self.d4c_get_azure_user_scripts_attachment() is True
 
     def test_GetCSPMAzureUserScripts(self):
-        assert self.serviceD4C_GetCSPMAzureUserScripts() is True
+        """
+        Pytest harness hook
+        """
+        assert self.d4c_get_azure_user_scripts() is True
 
     def test_GetCSPMGCPUserScriptsAttachment(self):
-        assert self.serviceD4C_GetCSPMGCPUserScriptsAttachment() is True
+        """
+        Pytest harness hook
+        """
+        assert self.d4c_get_gcp_user_scripts_attachment() is True
 
     def test_GetCSPMGCPUserScripts(self):
-        assert self.serviceD4C_GetCSPMGCPUserScripts() is True
+        """
+        Pytest harness hook
+        """
+        assert self.d4c_get_gcp_user_scripts() is True
 
     def test_Errors(self):
-        assert self.serviceD4C_GenerateErrors() is True
+        """
+        Pytest harness hook
+        """
+        assert self.d4c_generate_errors() is True
+
+    @staticmethod
+    def test_logout():
+        """
+        Pytest harness hook
+        """
+        assert bool(falcon.auth_object.revoke(
+            falcon.auth_object.token()["body"]["access_token"]
+            )["status_code"] in AllowedResponses) is True
