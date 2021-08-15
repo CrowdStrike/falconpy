573ed4c00cc9cca6cff5bf2e760f0c73b18f1d81
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-08-14 22:19:35 -0400

    Refactored to the latest pattern. Closes #298.

--- src/falconpy/malquery.py
+++ src/falconpy/malquery.py
@@ -36,177 +36,154 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <https://unlicense.org>
 """
-# pylint: disable=C0103  # Aligning method names to API operation IDs
-from ._util import service_request, force_default, args_to_params
+from ._util import process_service_request, force_default, handle_single_argument
 from ._service_class import ServiceClass
 from ._endpoint._malquery import _malquery_endpoints as Endpoints
 
 
 class MalQuery(ServiceClass):
-    """The only requirement to instantiate an instance of this class
-       is a valid token provided by the Falcon API SDK OAuth2 class, a
-       existing instance of the authentication class as an object or a
-       valid set of credentials.
     """
-    def GetMalQueryQuotasV1(self: object) -> dict:
+    The only requirement to instantiate an instance of this class
+    is a valid token provided by the Falcon API SDK OAuth2 class, a
+    existing instance of the authentication class as an object or a
+    valid set of credentials.
+    """
+    def get_quotas(self: object) -> dict:
         """Get information about search and download quotas in your environment"""
         # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/GetMalQueryQuotasV1
-        operation_id = "GetMalQueryQuotasV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
-        header_payload = self.headers
-        returned = service_request(caller=self,
-                                   method="GET",
-                                   endpoint=target_url,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            operation_id="GetMalQueryQuotasV1"
+            )
 
-    def PostMalQueryFuzzySearchV1(self: object, body: dict) -> dict:
-        """Search Falcon MalQuery quickly, but with more potential for false positives.
-           Search for a combination of hex patterns and strings in order to identify
-           samples based upon file content at byte level granularity.
+    def fuzzy_search(self: object, body: dict) -> dict:
+        """
+        Search Falcon MalQuery quickly, but with more potential for false positives.
+        Search for a combination of hex patterns and strings in order to identify
+        samples based upon file content at byte level granularity.
         """
         # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/PostMalQueryFuzzySearchV1
-        operation_id = "PostMalQueryFuzzySearchV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
-        header_payload = self.headers
-        body_payload = body
-        returned = service_request(caller=self,
-                                   method="POST",
-                                   endpoint=target_url,
-                                   body=body_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            method="POST",
+            operation_id="PostMalQueryFuzzySearchV1",
+            body=body
+            )
 
     @force_default(defaults=["parameters"], default_types=["dict"])
-    def GetMalQueryDownloadV1(self: object, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
-        """Download a file indexed by MalQuery. Specify the file using its SHA256.
-           Only one file is supported at this time.
+    def get_download(self: object, *args, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
+        """
+        Download a file indexed by MalQuery. Specify the file using its SHA256.
+        Only one file is supported at this time.
         """
         # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/GetMalQueryDownloadV1
-        operation_id = "GetMalQueryDownloadV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}".replace("?ids={}", "")
-        header_payload = self.headers
-        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
-        returned = service_request(caller=self,
-                                   method="GET",
-                                   endpoint=target_url,
-                                   params=parameter_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            operation_id="GetMalQueryDownloadV1",
+            keywords=kwargs,
+            params=handle_single_argument(args, parameters, "ids")
+            )
 
     @force_default(defaults=["parameters"], default_types=["dict"])
-    def GetMalQueryMetadataV1(self: object, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
-        """Retrieve indexed files metadata by their hash"""
+    def get_metadata(self: object, *args, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
+        """
+        Retrieve indexed files metadata by their hash
+        """
         # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/GetMalQueryMetadataV1
-        operation_id = "GetMalQueryMetadataV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}".replace("?ids={}", "")
-        header_payload = self.headers
-        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
-        returned = service_request(caller=self,
-                                   method="GET",
-                                   endpoint=target_url,
-                                   params=parameter_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            operation_id="GetMalQueryMetadataV1",
+            keywords=kwargs,
+            params=handle_single_argument(args, parameters, "ids")
+            )
 
     @force_default(defaults=["parameters"], default_types=["dict"])
-    def GetMalQueryRequestV1(self: object, parameters: dict = None, **kwargs) -> dict:
-        """Check the status and results of an asynchronous request, such as hunt or exact-search.
-           Supports a single request id at this time.
+    def get_request(self: object, *args, parameters: dict = None, **kwargs) -> dict:
+        """
+        Check the status and results of an asynchronous request, such as hunt or exact-search.
+        Supports a single request id at this time.
         """
         # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/GetMalQueryRequestV1
-        operation_id = "GetMalQueryRequestV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}".replace("?ids={}", "")
-        header_payload = self.headers
-        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
-        returned = service_request(caller=self,
-                                   method="GET",
-                                   endpoint=target_url,
-                                   params=parameter_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            operation_id="GetMalQueryRequestV1",
+            keywords=kwargs,
+            params=handle_single_argument(args, parameters, "ids")
+            )
 
     @force_default(defaults=["parameters"], default_types=["dict"])
-    def GetMalQueryEntitiesSamplesFetchV1(self: object, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
-        """Fetch a zip archive with password 'infected' containing the samples.
-           Call this once the /entities/samples-multidownload request has finished processing
+    def get_samples(self: object, *args, parameters: dict = None, **kwargs) -> dict:  # pylint: disable=C0103
+        """
+        Fetch a zip archive with password 'infected' containing the samples.
+        Call this once the /entities/samples-multidownload request has finished processing
         """
         # [GET] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/GetMalQueryEntitiesSamplesFetchV1
-        operation_id = "GetMalQueryEntitiesSamplesFetchV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}".replace("?ids={}", "")
-        header_payload = self.headers
-        parameter_payload = args_to_params(parameters, kwargs, Endpoints, operation_id)
-        returned = service_request(caller=self,
-                                   method="GET",
-                                   endpoint=target_url,
-                                   params=parameter_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            operation_id="GetMalQueryEntitiesSamplesFetchV1",
+            keywords=kwargs,
+            params=handle_single_argument(args, parameters, "ids")
+            )
 
-    def PostMalQueryEntitiesSamplesMultidownloadV1(self: object, body: dict) -> dict:
-        """Schedule samples for download. Use the result id with the /request endpoint to check
-           if the download is ready after which you can call the /entities/samples-fetch to get the zip.
+    def samples_multidownload(self: object, body: dict) -> dict:
+        """
+        Schedule samples for download. Use the result id with the /request endpoint to check
+        if the download is ready after which you can call the /entities/samples-fetch to get the zip.
         """
         # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#
         #           /malquery/PostMalQueryEntitiesSamplesMultidownloadV1
-        operation_id = "PostMalQueryEntitiesSamplesMultidownloadV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
-        header_payload = self.headers
-        body_payload = body
-        returned = service_request(caller=self,
-                                   method="POST",
-                                   endpoint=target_url,
-                                   body=body_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            method="POST",
+            operation_id="PostMalQueryEntitiesSamplesMultidownloadV1",
+            body=body
+            )
 
-    def PostMalQueryExactSearchV1(self: object, body: dict) -> dict:
-        """Search Falcon MalQuery for a combination of hex patterns and strings in order to identify samples
-           based upon file content at byte level granularity. You can filter results on criteria such as file type,
-           file size and first seen date. Returns a request id which can be used with the /request endpoint.
+    def exact_search(self: object, body: dict) -> dict:
+        """
+        Search Falcon MalQuery for a combination of hex patterns and strings in order to identify samples
+        based upon file content at byte level granularity. You can filter results on criteria such as file type,
+        file size and first seen date. Returns a request id which can be used with the /request endpoint.
         """
         # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/PostMalQueryExactSearchV1
-        operation_id = "PostMalQueryExactSearchV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
-        header_payload = self.headers
-        body_payload = body
-        returned = service_request(caller=self,
-                                   method="POST",
-                                   endpoint=target_url,
-                                   body=body_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            method="POST",
+            operation_id="PostMalQueryExactSearchV1",
+            body=body
+            )
 
-    def PostMalQueryHuntV1(self: object, body: dict) -> dict:
-        """Schedule a YARA-based search for execution.
-           Returns a request id which can be used with the /request endpoint.
+    def hunt(self: object, body: dict) -> dict:
+        """
+        Schedule a YARA-based search for execution.
+        Returns a request id which can be used with the /request endpoint.
         """
         # [POST] https://assets.falcon.crowdstrike.com/support/api/swagger.html#/malquery/PostMalQueryHuntV1
-        operation_id = "PostMalQueryHuntV1"
-        target_url = f"{self.base_url}{[ep[2] for ep in Endpoints if operation_id in ep[0]][0]}"
-        header_payload = self.headers
-        body_payload = body
-        returned = service_request(caller=self,
-                                   method="POST",
-                                   endpoint=target_url,
-                                   body=body_payload,
-                                   headers=header_payload,
-                                   verify=self.ssl_verify
-                                   )
-        return returned
+        return process_service_request(
+            calling_object=self,
+            endpoints=Endpoints,
+            method="POST",
+            operation_id="PostMalQueryHuntV1",
+            body=body
+            )
+
+    # These method names align to the operation IDs in the API but
+    # do not conform to snake_case / PEP8 and are defined here for
+    # backwards compatibility / ease of use purposes
+    GetMalQueryQuotasV1 = get_quotas
+    PostMalQueryFuzzySearchV1 = fuzzy_search
+    GetMalQueryDownloadV1 = get_download
+    GetMalQueryMetadataV1 = get_metadata
+    GetMalQueryRequestV1 = get_request
+    GetMalQueryEntitiesSamplesFetchV1 = get_samples
+    PostMalQueryEntitiesSamplesMultidownloadV1 = samples_multidownload
+    PostMalQueryExactSearchV1 = exact_search
+    PostMalQueryHuntV1 = hunt
