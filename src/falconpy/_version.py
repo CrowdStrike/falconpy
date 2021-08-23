45cce723ebab1cf1d3ca0e22a77c14fe9bd3017b
jshcodes <74007258+jshcodes@users.noreply.github.com>
2021-08-23 16:12:04 -0400

    [DEPLOY] v0.6.2 - Uber class empty ids array fix (#315)
    * Fix Uber passing empty ids array. Closes #314.
    
    * Bump version -> 0.6.2
    
    * Update CHANGELOG.md
    
    * Fix typo

--- src/falconpy/_version.py
+++ src/falconpy/_version.py
@@ -36,7 +36,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <https://unlicense.org>
 """
-_VERSION = '0.6.1'
+_VERSION = '0.6.2'
 _MAINTAINER = 'Joshua Hiller'
 _AUTHOR = 'CrowdStrike'
 _AUTHOR_EMAIL = 'falconpy@crowdstrike.com'
