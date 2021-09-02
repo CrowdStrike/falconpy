8b57bfce9244e09e602b935e0cbc3afede3c33fd
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-09-02 13:31:30 -0400

    Fix typo

--- samples/hosts/stale_sensors.py
+++ samples/hosts/stale_sensors.py
@@ -12,7 +12,7 @@ try:
     from falconpy.hosts import Hosts
 except ImportError as no_falconpy:
     raise SystemExit(
-        "CrowdStrike FalconPy must be installed in order to use the application.\n"
+        "CrowdStrike FalconPy must be installed in order to use this application.\n"
         "Please execute `python3 -m pip install crowdstrike-falconpy and try again."
         ) from no_falconpy
 
