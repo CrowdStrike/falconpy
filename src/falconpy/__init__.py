e61af2a1ea28db4b3b0b9d6106fb1a76a30346fe
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-09-20 14:35:29 -0400

    Package adjustment - PEP8 classes easier to import

--- src/falconpy/__init__.py
+++ src/falconpy/__init__.py
@@ -22,6 +22,50 @@
 from ._version import _VERSION, _MAINTAINER, _AUTHOR, _AUTHOR_EMAIL
 from ._version import _CREDITS, _DESCRIPTION, _TITLE, _PROJECT_URL
 from ._version import _DOCS_URL, _KEYWORDS
+from .api_complete import APIHarness
+from .cloud_connect_aws import CloudConnectAWS
+from .cspm_registration import CSPMRegistration
+from .custom_ioa import CustomIOA
+from .d4c_registration import D4CRegistration
+from .detects import Detects
+from .device_control_policies import DeviceControlPolicies
+from .event_streams import EventStreams
+from .falcon_complete_dashboard import CompleteDashboard
+from .falcon_container import FalconContainer
+from .falconx_sandbox import FalconXSandbox
+from .firewall_management import FirewallManagement
+from .firewall_policies import FirewallPolicies
+from .host_group import HostGroup
+from .hosts import Hosts
+from .identity_protection import IdentityProtection
+from .incidents import Incidents
+from .installation_tokens import InstallationTokens
+from .intel import Intel
+from .ioa_exclusions import IOAExclusions
+from .ioc import IOC
+from .iocs import Iocs
+from .kubernetes_protection import KubernetesProtection
+from .malquery import MalQuery
+from .ml_exclusions import MLExclusions
+from .mssp import FlightControl
+from .oauth2 import OAuth2
+from .overwatch_dashboard import OverwatchDashboard
+from .prevention_policy import PreventionPolicy
+from .quarantine import Quarantine
+from .quick_scan import QuickScan
+from .real_time_response_admin import RealTimeResponseAdmin
+from .real_time_response import RealTimeResponse
+from .recon import Recon
+from .report_executions import ReportExecutions
+from .response_policies import ResponsePolicies
+from .sample_uploads import SampleUploads
+from .scheduled_reports import ScheduledReports
+from .sensor_download import SensorDownload
+from .sensor_update_policy import SensorUpdatePolicy
+from .sensor_visibility_exclusions import SensorVisibilityExclusions
+from .spotlight_vulnerabilities import SpotlightVulnerabilities
+from .user_management import UserManagement
+from .zero_trust_assessment import ZeroTrustAssessment
 
 __version__ = _VERSION
 __maintainer__ = _MAINTAINER
@@ -33,7 +77,17 @@ __title__ = _TITLE
 __project_url__ = _PROJECT_URL
 __docs_url__ = _DOCS_URL
 __keywords__ = _KEYWORDS
-
+__all__ = [
+    "APIHarness", "CloudConnectAWS", "CSPMRegistration", "CustomIOA", "D4CRegistration",
+    "Detects", "DeviceControlPolicies", "EventStreams", "CompleteDashboard",
+    "FalconContainer", "FalconXSandbox", "FirewallManagement", "FirewallPolicies", "HostGroup",
+    "Hosts", "IdentityProtection", "Incidents", "InstallationTokens", "Intel", "IOAExclusions",
+    "IOC", "Iocs", "KubernetesProtection", "MalQuery", "MLExclusions", "FlightControl", "OAuth2",
+    "OverwatchDashboard", "PreventionPolicy", "Quarantine", "QuickScan", "RealTimeResponseAdmin",
+    "RealTimeResponse", "Recon", "ReportExecutions", "ResponsePolicies", "SampleUploads",
+    "ScheduledReports", "SensorDownload", "SensorUpdatePolicy", "SensorVisibilityExclusions",
+    "SpotlightVulnerabilities", "UserManagement", "ZeroTrustAssessment"
+ ]
 """
 This is free and unencumbered software released into the public domain.
 
