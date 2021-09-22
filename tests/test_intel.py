602021e9c7dc20299211648b7c272eecf69839e5
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-09-22 10:56:34 -0400

    Fix unit test - Validation failure

--- tests/test_intel.py
+++ tests/test_intel.py
@@ -1,6 +1,4 @@
-"""
-test_intel.py - This class tests the intel service class
-"""
+"""This class tests the intel service class"""
 import os
 import sys
 import pytest
@@ -18,32 +16,29 @@ AllowedResponses = [200, 201, 400, 404, 429]
 
 
 class TestIntel:
-    """
-    Intel Service Class test harness
-    """
+    """Intel Service Class test harness"""
     def intel_test_all_code_paths(self):
-        """
-        Tests every statement within every method of the class, accepts all errors except 500
-        """
+        """Tests every statement within every method of the class, accepts all errors except 500"""
         error_checks = True
         tests = {
-            "query_intel_actor_entities": falcon.QueryIntelActorEntities(limit=1)["status_code"],
-            "query_intel_indicator_entities": falcon.QueryIntelIndicatorEntities(parameters={"limit": 1})["status_code"],
-            "query_intel_report_entities": falcon.QueryIntelReportEntities()["status_code"],
-            "get_intel_actor_entities": falcon.GetIntelActorEntities(ids='12345678')["status_code"],
-            "get_intel_indicator_entities": falcon.GetIntelIndicatorEntities(body={})["status_code"],
-            "get_intel_report_pdf": falcon.GetIntelReportPDF(parameters={})["status_code"],
-            "get_intel_report_entities": falcon.GetIntelReportEntities(ids='12345678')["status_code"],
-            "get_intel_rule_file": falcon.GetIntelRuleFile(parameters={})["status_code"],
-            "get_latest_intel_rule_file": falcon.GetLatestIntelRuleFile(parameters={})["status_code"],
-            "get_intel_rule_entities": falcon.GetIntelRuleEntities(ids='12345678')["status_code"],
-            "query_intel_actor_ids": falcon.QueryIntelActorIds()["status_code"],
-            "query_intel_indicator_ids": falcon.QueryIntelIndicatorIds(limit=5)["status_code"],
-            "query_intel_report_ids": falcon.QueryIntelReportIds(limit=1)["status_code"],
-            "query_intel_rule_ids": falcon.QueryIntelRuleIds(parameters={"type": "common-event-format"})["status_code"]
+            "query_intel_actor_entities": falcon.QueryIntelActorEntities(limit=1),
+            "query_intel_indicator_entities": falcon.QueryIntelIndicatorEntities(parameters={"limit": 1}),
+            "query_intel_report_entities": falcon.QueryIntelReportEntities(),
+            "get_intel_actor_entities": falcon.GetIntelActorEntities(ids='12345678'),
+            "get_intel_indicator_entities": falcon.GetIntelIndicatorEntities(body={"ids":["1234567"]}),
+            "get_intel_report_pdf": falcon.GetIntelReportPDF(parameters={}),
+            "get_intel_report_entities": falcon.GetIntelReportEntities(ids='12345678'),
+            "get_intel_rule_file": falcon.GetIntelRuleFile(parameters={}),
+            "get_latest_intel_rule_file": falcon.GetLatestIntelRuleFile(parameters={}),
+            "get_intel_rule_entities": falcon.GetIntelRuleEntities(ids='12345678'),
+            "query_intel_actor_ids": falcon.QueryIntelActorIds(),
+            "query_intel_indicator_ids": falcon.QueryIntelIndicatorIds(limit=5),
+            "query_intel_report_ids": falcon.QueryIntelReportIds(limit=1),
+            "query_intel_rule_ids": falcon.QueryIntelRuleIds(parameters={"type": "common-event-format"})
         }
         for key in tests:
-            if tests[key] not in AllowedResponses:
+            if tests[key]["status_code"] not in AllowedResponses:
+                print(tests[key])    
                 error_checks = False
 
             # print(f"{key} operation returned a {tests[key]} status code")
@@ -52,10 +47,3 @@ class TestIntel:
 
     def test_all_code_paths(self):
         assert self.intel_test_all_code_paths() is True
-
-    # @staticmethod
-    # def test_logout():
-    #     """Pytest harness hook"""
-    #     assert bool(falcon.auth_object.revoke(
-    #         falcon.auth_object.token()["body"]["access_token"]
-    #         )["status_code"] in AllowedResponses) is True
