fb088041135f2b110ca2f2cdb87cf08416c29620
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-09-24 12:26:27 -0400

    Updated unit testing to cover new parameters.

--- tests/test_sample_uploads.py
+++ tests/test_sample_uploads.py
@@ -11,16 +11,16 @@ from tests import test_authorization as Authorization
 # Import our sibling src folder into the path
 sys.path.append(os.path.abspath('src'))
 # Classes to test - manually imported from sibling folder
-from falconpy.sample_uploads import Sample_Uploads
+from falconpy import SampleUploads
 
 auth = Authorization.TestAuthorization()
 token = auth.getConfigExtended()
-falcon = Sample_Uploads(access_token=token)
+falcon = SampleUploads(access_token=token)
 AllowedResponses = [200, 201, 400, 404, 429]
 
 
 class TestSampleUploads:
-    def sample_upload_download_delete(self):
+    def sample_upload_download_delete(self, style: str = "file_data"):
         """
         Tests all functionality within the class by performing an upload / download / compare / delete.
         """
@@ -34,7 +34,12 @@ class TestSampleUploads:
         SOURCE = "%s_source.png" % jdate
         TARGET = "tests/%s_target.png" % jdate
         PAYLOAD = open(FILENAME, 'rb').read()
-        response = falcon.UploadSampleV3(file_name=SOURCE, file_data=PAYLOAD)
+        if style.lower() == "sample":
+            response = falcon.UploadSampleV3(file_name=SOURCE, sample=PAYLOAD)
+        elif style.lower() == "upfile":
+            response = falcon.UploadSampleV3(file_name=SOURCE, upfile=PAYLOAD)
+        else:
+            response = falcon.UploadSampleV3(file_name=SOURCE, file_data=PAYLOAD)
         try:
             sha = response["body"]["resources"][0]["sha256"]
         except KeyError:
@@ -44,7 +49,8 @@ class TestSampleUploads:
             try:
                 open(TARGET, 'wb').write(response)
             except TypeError:
-                # This particular unit test failed it's upload, pass a True since the code path was tested
+                # This particular unit test failed it's upload,
+                # pass a True since the code path was tested
                 pytest.skip("Unable to open test file, skipping.")
                 return True
             buf = 65536
@@ -83,29 +89,31 @@ class TestSampleUploads:
         """
         error_checks = True
         tests = {
-            "upload_sample": falcon.UploadSampleV3(file_data={}, file_name='oops_I_broke_it.jpg')["status_code"],
-            "get_sample": falcon.GetSampleV3(ids='DoesNotExist')["status_code"],
-            "delete_sample": falcon.DeleteSampleV3(ids='12345678')["status_code"]
+            "upload_sample": falcon.UploadSampleV3(body={}),
+            "get_sample": falcon.GetSampleV3(ids='DoesNotExist'),
+            "delete_sample": falcon.DeleteSampleV3(ids='12345678')
         }
         for key in tests:
-            if tests[key] not in AllowedResponses:
+            if tests[key]["status_code"] not in AllowedResponses:
+                # print(tests[key])
                 error_checks = False
 
             # print(f"{key} operation returned a {tests[key]} status code")
 
         return error_checks
 
-    def test_all_functionality(self):
+    def test_all_functionality_file_data(self):
         """Pytest harness hook"""
-        assert self.sample_upload_download_delete() is True
+        assert self.sample_upload_download_delete("file_data") is True
+
+    def test_all_functionality_sample(self):
+        """Pytest harness hook"""
+        assert self.sample_upload_download_delete("sample") is True
+
+    def test_all_functionality_upfile(self):
+        """Pytest harness hook"""
+        assert self.sample_upload_download_delete("upfile") is True
 
     def test_errors(self):
         """Pytest harness hook"""
         assert self.sample_errors() is True
-
-    # @staticmethod
-    # def test_logout():
-    #     """Pytest harness hook"""
-    #     assert bool(falcon.auth_object.revoke(
-    #         falcon.auth_object.token()["body"]["access_token"]
-    #         )["status_code"] in AllowedResponses) is True
