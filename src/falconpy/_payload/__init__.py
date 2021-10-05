7ae619475f78f6bd550fa1dad0146b7931efd729
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-10-05 18:10:11 -0400

    Add prevention policy payload handler

--- src/falconpy/_payload/__init__.py
+++ src/falconpy/_payload/__init__.py
@@ -27,11 +27,13 @@ from ._recon import recon_notifications_payload, recon_rule_preview_payload
 from ._malquery import malquery_exact_search_payload, malquery_hunt_payload, malquery_fuzzy_payload
 from ._detects import update_detects_payload
 from ._ioa import ioa_exclusion_payload
+from ._prevention_policy import prevention_policy_payload
 
 __all__ = [
     "generic_payload_list", "aggregate_payload", "recon_action_payload", "recon_rules_payload",
     "recon_action_update_payload", "recon_notifications_payload", "recon_rule_preview_payload",
     "malquery_exact_search_payload", "malquery_hunt_payload", "malquery_fuzzy_payload",
     "update_detects_payload", "exclusion_payload", "ioa_exclusion_payload",
-    "host_group_create_payload", "host_group_update_payload", "installation_token_payload"
+    "host_group_create_payload", "host_group_update_payload", "installation_token_payload",
+    "prevention_policy_payload"
 ]
