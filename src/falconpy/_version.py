d25f03e2be0e727ce4c9161c90cd7eedb90c7df0
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-11-08 14:12:01 -0500

    Revert "Bump version -> 0.8.2"
    This reverts commit 81e9a614e270e37ac6bc298fa2908f099753a53c.

--- src/falconpy/_version.py
+++ src/falconpy/_version.py
@@ -35,7 +35,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <https://unlicense.org>
 """
-_VERSION = '0.8.2'
+_VERSION = '0.8.1'
 _MAINTAINER = 'Joshua Hiller'
 _AUTHOR = 'CrowdStrike'
 _AUTHOR_EMAIL = 'falconpy@crowdstrike.com'
