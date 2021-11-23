efe41d9d5c5cbf1f2fc1017a2f9f53a9dfec821f
Joshua Hiller <74007258+jshcodes@users.noreply.github.com>
2021-11-23 16:19:13 -0500

    Refactored to leverage new library functionality. (#441)

--- samples/hosts/sensor_versions_by_hostname.py
+++ samples/hosts/sensor_versions_by_hostname.py
@@ -1,55 +1,89 @@
+"""This sample demonstrates retrieving sensor versions by hostname.
+
+This example requires FalconPy v0.7.0+
 """
-This sample demonstrates retrieving sensor versions by hostname
-"""
-# pylint: disable=C0103,W0621,E0401
-import json
-from falconpy.hosts import Hosts
+import argparse
+from falconpy import Hosts
 
 
-def device_list(offset: int, limit: int):
-    """
-    I return a list of all devices for the CID, if I max out on the query limit, I can paginate
-    """
-    result = falcon.QueryDevicesByFilter(parameters={"limit": limit, "offset": offset})
+def parse_command_line() -> object:
+    """Parses command-line arguments and returns them back as an object."""
+    parser = argparse.ArgumentParser(description="List sensors versions by hostname")
+    parser.add_argument(
+        '-k',
+        '--client_id',
+        help='CrowdStrike Falcon API key ID',
+        required=True
+        )
+    parser.add_argument(
+        '-s',
+        '--client_secret',
+        help='CrowdStrike Falcon API key secret',
+        required=True
+        )
+    parser.add_argument(
+        '-b',
+        '--base_url',
+        help='CrowdStrike API region (us1, us2, eu1, usgov1)',
+        required=False
+    )
+    parser.add_argument(
+        '-r',
+        '--reverse',
+        help='Reverse sort (defaults to ASC)',
+        required=False,
+        action="store_true"
+        )
+
+    return parser.parse_args()
+
+
+def device_list(off: int, limit: int, sort: str):
+    """Return a list of all devices for the CID, paginating when necessary."""
+    result = falcon.QueryDevicesByFilter(limit=limit, offset=off, sort=sort)
     new_offset = result["body"]["meta"]["pagination"]["offset"]
     total = result["body"]["meta"]["pagination"]["total"]
-    device_list = result["body"]["resources"]
-    return new_offset, total, device_list
+    returned_device_list = result["body"]["resources"]
+    return new_offset, total, returned_device_list
 
 
 def device_detail(aids: list):
-    """
-    I return the device_id and agent_version for a list of AIDs I'm provided
-    """
+    """Return the device_id and agent_version for a list of AIDs provided."""
     result = falcon.GetDeviceDetails(ids=aids)
-    devices = []
+    device_details = []
     # return just the aid and agent version
     for device in result["body"]["resources"]:
         res = {}
         res["hostname"] = device.get("hostname", None)
         res["agent_version"] = device.get("agent_version", None)
-        devices.append(res)
-    return devices
+        device_details.append(res)
+    return device_details
 
 
-# Grab our config parameters from a local file.
-with open('../config.json', 'r') as file_config:
-    config = json.loads(file_config.read())
+args = parse_command_line()
 
-falcon = Hosts(creds={
-        "client_id": config["falcon_client_id"],
-        "client_secret": config["falcon_client_secret"]
-    },
-    # base_url = "https://YOUR_BASE_URL.crowdstrike.com"   # Enter your base URL here if it is not US-1
-)
+if args.base_url:
+    BASE = args.base_url
+else:
+    BASE = "us1"
 
-offset = 0      # Start at the beginning
-displayed = 0   # This is just so we can show a running count
-total = 1       # Assume there is at least one
-limit = 500     # Quick limit to prove pagination
-while offset < total:
-    offset, total, devices = device_list(offset, limit)
+if args.reverse:
+    SORT = "hostname.desc"
+else:
+    SORT = "hostname.asc"
+
+falcon = Hosts(client_id=args.client_id,
+               client_secret=args.client_secret,
+               base_url=BASE
+               )
+
+OFFSET = 0      # Start at the beginning
+DISPLAYED = 0   # This is just so we can show a running count
+TOTAL = 1       # Assume there is at least one
+LIMIT = 500     # Quick limit to prove pagination
+while OFFSET < TOTAL:
+    OFFSET, TOTAL, devices = device_list(OFFSET, LIMIT, SORT)
     details = device_detail(devices)
     for detail in details:
-        displayed += 1
-        print(f"{displayed}: {detail['hostname']} is on version {detail['agent_version']}")
+        DISPLAYED += 1
+        print(f"{DISPLAYED}: {detail['hostname']} is on version {detail['agent_version']}")
