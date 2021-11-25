c03405935d1b142f3d034591f6e1018c1d175993
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-11-25 09:50:26 -0500

    Update unit testing

--- tests/test_ioc.py
+++ tests/test_ioc.py
@@ -8,7 +8,7 @@ from tests import test_authorization as Authorization
 # Import our sibling src folder into the path
 sys.path.append(os.path.abspath('src'))
 # Classes to test - manually imported from sibling folder
-from falconpy.ioc import IOC
+from falconpy import IOC
 
 auth = Authorization.TestAuthorization()
 token = auth.getConfigExtended()
