3a10dc252fa9f8e3bdfaa3548e57636d1b8e4687
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-12-04 09:00:32 -0500

    Refactored to latest usage patterns.

--- samples/ioc/create_ioc.py
+++ samples/ioc/create_ioc.py
@@ -9,31 +9,40 @@
 
 Create IOC Example - @jshcodes 06.23.21
 
-FalconPy v.0.5.0+ only, uses the new IOC endpoint
+FalconPy v.0.8.6+
 """
 import json
-from falconpy.api_complete import APIHarness as Uber
-from falconpy.ioc import IOC as IOC
+from falconpy import APIHarness, IOC
 
 
 def connectAPI(class_type: str = "service"):
-    with open("../config.json", "r") as cred_file:
+    """Connect to the selected API service."""
+    with open("../config.json", "r", encoding="utf-8") as cred_file:
         config = json.loads(cred_file.read())
-    creds = {
-        "client_id": config["falcon_client_id"],
-        "client_secret": config["falcon_client_secret"]
-    }
+
     if class_type.lower() == "service":
-        falcon = IOC(creds=creds)
+        falcon = IOC(client_id=config["falcon_client_id"],
+                     client_secret=config["falcon_client_secret"]
+                     )
     elif class_type.lower() == "uber":
-        falcon = Uber(creds=creds)
+        falcon = APIHarness(client_id=config["falcon_client_id"],
+                            client_secret=config["falcon_client_secret"]
+                            )
     else:
         falcon = None
 
     return falcon
 
 
-def createIOCPayload(source: str, action: str, expiration: str, desc: str, type: str, val: str, platforms: list):
+def createIOCPayload(source: str,
+                     action: str,
+                     expiration: str,
+                     desc: str,
+                     type: str,
+                     val: str,
+                     platforms: list,
+                     severity: str
+                     ) -> dict:
     payload = {
         "indicators": [
             {
@@ -44,6 +53,7 @@ def createIOCPayload(source: str, action: str, expiration: str, desc: str, type:
                 "type": type,
                 "value": val,
                 "platforms": platforms,
+                "severity": severity,
                 "applied_globally": True
             }
         ]
@@ -53,12 +63,22 @@ def createIOCPayload(source: str, action: str, expiration: str, desc: str, type:
 
 # Create an IOC using the IOC Service class
 falcon = connectAPI("service")
-BODY = createIOCPayload("Test", "detect", "2021-06-30T05:00:00.000Z", "Testing", "ipv4", "9.8.7.6", ["linux"])
-response = falcon.indicator_create_v1(body=BODY)
+# Since we are using a version greater than 0.7.3, we are able to make use of Body Payload abstraction
+response = falcon.indicator_create(source="Test",
+                                   action="detect",
+                                   expiration="2021-12-05T05:00:00.000Z",
+                                   description="Testing",
+                                   type="ipv4",
+                                   value="8.7.6.5",
+                                   severity="LOW",
+                                   applied_globally=True,
+                                   platforms="linux"    # Can be a list or a comma-delimited string. This is a list of 1.
+                                   )
 print(response)
 
 # Create an IOC using the Uber class
 falcon = connectAPI("uber")
-BODY = createIOCPayload("Test", "detect", "2021-06-30T05:00:00.000Z", "Testing", "ipv4", "8.7.6.5", ["linux"])
+# The Uber class does not support Body Payload abstraction. We will send it a standard body payload instead.
+BODY = createIOCPayload("Test", "detect", "2021-12-05T05:00:00.000Z", "Testing", "ipv4", "3.4.5.6", ["linux"], "LOW")
 response = falcon.command('indicator_create_v1', body=BODY)
 print(response)
