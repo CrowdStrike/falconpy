09b9196035b2c3bcd60a58bf928567305075dcc4
Joshua Hiller <74007258+jshcodes@users.noreply.github.com>
2021-12-23 02:32:05 -0500

    Update create-lambda-layer.sh

--- util/create-lambda-layer.sh
+++ util/create-lambda-layer.sh
@@ -4,16 +4,12 @@
 # as an AWS lambda layer.
 
 # Tested on Darwin and AWS Linux 2
-
+# Assumes the docker service is running.
+#
 # Currently targets Python 3.8
 echo "crowdstrike-falconpy" > lambda-requirements.txt
 # Remove any old copies
 rm falconpy-layer.zip >/dev/null 2>&1
-# Fire up docker
-docker-machine start
-docker-machine env default
-# Connect to the default environment
-eval $(docker-machine env default)
 # Run the lambci image and install the requirements
 docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8 \
 pip install -r lambda-requirements.txt -t python
