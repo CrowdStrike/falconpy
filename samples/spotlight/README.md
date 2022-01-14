634d30c7280886dee8450eb9dc9461cd10dc9519
Joshua Hiller <74007258+jshcodes@users.noreply.github.com>
2022-01-13 20:14:34 -0500

    Spotlight sample update (#518)
    * Refactor find hosts by CVE example
    
    * Add usage detail for Spotlight sample
    
    * Fix typo
    
    * Update wordlist.txt

--- samples/spotlight/README.md
+++ samples/spotlight/README.md
@@ -4,4 +4,140 @@
 # Falcon Spotlight Vulnerabilities samples
 The examples within this folder focus on leveraging CrowdStrike's Falcon Spotlight Vulnerabilities API.
 
-- [Identify hosts with vulnerabilities by CVE](find_hosts_by_cve.py)
+- [Identify hosts with vulnerabilities by CVE](#identify-hosts-with-vulnerabilities-by-cve)
+
+
+## Identify hosts with vulnerabilities by CVE
+Retrieves a list of hosts with vulnerabilities matching the CVE(s) specified. Also provides remediation recommendations when available.
+
+### Dependencies
+This sample is dependent upon the [`python-tabulate`](https://github.com/gregbanks/python-tabulate) library.
+
+#### Installing tabulate
+Tabulate can be installed using the Python Package Index:
+
+```shell
+python3 -m pip install tabulate
+```
+
+### Running the program
+In order to run this demonstration, you you will need access to CrowdStrike API keys with the following scopes:
+
+| Service Collection | Scope |
+| :---- | :---- |
+| Hosts | __READ__ |
+| Spotlight Vulnerabilities | __READ__ |
+
+### Execution syntax
+The following command will retrieve a list of hosts matching the specified CVE.
+
+```shell
+python3 find_hosts_by_cve.py -k $FALCON_CLIENT_ID -s $FALCON_CLIENT_SECRET -c 2021-22947
+```
+
+> You do not need to prepend the `CVE-` string to your CVE ID. Both formats are accepted.
+
+> You can search for multiple CVEs by passing a comma delimited string for the `-c` argument.
+
+You can filter columns from the result display using the `-x` argument.
+
+```shell
+python3 find_hosts_by_cve.py -k $FALCON_CLIENT_ID -s $FALCON_CLIENT_SECRET -c CVE-2021-22947 -x cve_description
+```
+
+To show a progress indicator, use the `-p` option.
+
+```shell
+python3 find_hosts_by_cve.py -k $FALCON_CLIENT_ID -s $FALCON_CLIENT_SECRET -c 2021-22947 -p
+```
+
+Sort results by creation date (`asc` / `desc`) using the `-o` argument.
+
+```shell
+python3 find_hosts_by_cve.py -k $FALCON_CLIENT_ID -s $FALCON_CLIENT_SECRET -c 2021-22947 -o desc
+```
+
+Multiple table formats are supported with the `-f` argument.
+
+```shell
+python3 find_hosts_by_cve.py -k $FALCON_CLIENT_ID -s $FALCON_CLIENT_SECRET -c 2021-22947 -f simple
+```
+
+##### Available table formats
+- plain
+- simple
+- github
+- grid (Default)
+- fancy_grid
+- pipe
+- orgtbl
+- jira
+- presto
+- pretty
+- psql
+- rst
+- mediawiki
+- moinmoin
+- youtrack
+- html
+- unsafehtml
+- latex
+- latex_raw
+- latex_booktabs
+- latex_longtable
+- textile
+- tsv
+
+
+#### Command-line help
+Command-line help is available via the `-h` argument.
+
+```shell
+python3 find_hosts_by_cve.py -h
+usage: find_hosts_by_cve.py [-h] -k CLIENT_ID -s CLIENT_SECRET [-b BASE_URL] -c CVE [-x EXCLUDE] [-f FORMAT] [-o SORT] [-p]
+
+Retrieve hosts by CVE vulnerability.
+
+ ______                         __ _______ __         __ __
+|      |.----.-----.--.--.--.--|  |     __|  |_.----.|__|  |--.-----.
+|   ---||   _|  _  |  |  |  |  _  |__     |   _|   _||  |    <|  -__|
+|______||__| |_____|________|_____|_______|____|__|  |__|__|__|_____|
+                     _______             __   __ __       __    __
+                    |   _   .-----.-----|  |_|  |__.-----|  |--|  |_
+                    |   1___|  _  |  _  |   _|  |  |  _  |     |   _|
+                    |____   |   __|_____|____|__|__|___  |__|__|____|
+                    |:  1   |__|                   |_____|
+                    |::.. . |
+                    `-------'               Find hosts by CVE
+
+Creation date: 01.13.2021 - jshcodes@CrowdStrike
+
+This solution requires crowdstrike-falconpy v0.8.6+ and tabulate
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k CLIENT_ID, --client_id CLIENT_ID
+                        CrowdStrike Falcon API key ID
+  -s CLIENT_SECRET, --client_secret CLIENT_SECRET
+                        CrowdStrike Falcon API key secret
+  -b BASE_URL, --base_url BASE_URL
+                        CrowdStrike API region (us1, us2, eu1, usgov1)
+                        NOT required unless you are using `usgov1`
+  -c CVE, --cve CVE     CVE IDs to search for. (ex: CVE-2022-12345,CVE-2022-54321)
+                        Delimit with a comma (no spaces). The string CVE- is not required.
+  -x EXCLUDE, --exclude EXCLUDE
+                        List of columns to exclude from the display.
+                        Delimit with a comma (no spaces).
+                        (cve, score, severity, cve_description, created_on, updated_on,
+                        hostname, local_ip, os_version, service_provider, remediation)
+  -f FORMAT, --format FORMAT
+                        Table format to use for display.
+                        (plain, simple, github, grid, fancy_grid, pipe, orgtbl, jira, presto,
+                        pretty, psql, rst, mediawiki, moinmoin, youtrack, html, unsafehtml,
+                        latext, latex_raw, latex_booktabs, latex_longtable, textile, tsv)
+  -o SORT, --sort SORT  Sort results by creation time (asc or desc).
+  -p, --show_progress   Show a progress indicator as data is retrieved.
+```
+
+### Example source code
+The source code for this example can be found [here](find_hosts_by_cve.py).
--- samples/spotlight/find_hosts_by_cve.py
+++ samples/spotlight/find_hosts_by_cve.py
@@ -1,14 +1,5 @@
-"""Retrieve hosts by CVE vulnearbility.
+"""Retrieve hosts by CVE vulnerability.
 
-Creation date: 01.13.2021 - jshcodes@CrowdStrike
-
-This solution requires crowdstrike-falconpy v0.8.6+
-"""
-from argparse import ArgumentParser, RawTextHelpFormatter
-from tabulate import tabulate
-from falconpy import SpotlightVulnerabilities, Hosts, OAuth2
-
-BANNER = """
  ______                         __ _______ __         __ __
 |      |.----.-----.--.--.--.--|  |     __|  |_.----.|__|  |--.-----.
 |   ---||   _|  _  |  |  |  |  _  |__     |   _|   _||  |    <|  -__|
@@ -20,7 +11,103 @@ BANNER = """
                     |:  1   |__|                   |_____|
                     |::.. . |
                     `-------'               Find hosts by CVE
+
+Creation date: 01.13.2021 - jshcodes@CrowdStrike
+
+This solution requires crowdstrike-falconpy v0.8.6+ and tabulate
 """
+from argparse import ArgumentParser, RawTextHelpFormatter
+import json
+from tabulate import tabulate
+from falconpy import SpotlightVulnerabilities, Hosts, OAuth2
+
+
+class SpotlightCVEMatch():  # pylint: disable=R0902
+    """Class to represent a returned match to a CVE vulnerability."""
+
+    def __init__(self, api_response):  # pylint: disable=R0912
+        """Initialize the object and set all attributes based upon the inbound API response."""
+        self.cve = None
+        self.score = 0
+        self.severity = None
+        self.cve_description = None
+        self.created_on = None
+        self.updated_on = None
+        self.hostname = "Unknown"
+        self.local_ip = "Unknown"
+        self.os_version = "Unknown"
+        self.service_provider = "Unknown"
+        self.remediation = "Not found"
+
+        cve_detail = api_response["cve"]
+        if "id" in cve_detail:
+            self.cve = cve_detail["id"]
+        if "base_score" in cve_detail:
+            self.score = cve_detail["base_score"]
+        if "severity" in cve_detail:
+            self.severity = cve_detail["severity"]
+        if "created_timestamp" in api_response:
+            self.created_on = api_response["created_timestamp"]
+        if "updated_timestamp" in api_response:
+            self.updated_on = api_response["updated_timestamp"]
+
+        host_detail = hosts.get_device_details(ids=api_response["aid"])["body"]["resources"]
+        if host_detail:
+            for host in host_detail:
+                if "hostname" in host:
+                    self.hostname = host["hostname"]
+                if "local_ip" in host:
+                    self.local_ip = host["local_ip"]
+                if "os_version" in host:
+                    self.os_version = host["os_version"]
+                if "service_provider" in host:
+                    self.service_provider = host["service_provider"]
+        else:
+            # No host details are returned, show the AID for this entry
+            self.hostname = api_response["aid"]
+
+        try:
+            self.remediation = self.chunk_long_description(
+                    spotlight.get_remediations_v2(
+                        ids=api_response["remediation"]["ids"]
+                        )["body"]["resources"][0]["action"],
+                    30
+                    )
+        except KeyError:
+            pass
+
+        if not self.remediation:
+            self.remediation = "Not found"
+
+        self.cve_description = self.chunk_long_description(
+            api_response["cve"]["description"].strip(),
+            15
+            )
+
+    @staticmethod
+    def chunk_long_description(desc, col_width) -> str:
+        """Chunks a long string by delimiting with CR based upon column length."""
+        desc_chunks = []
+        chunk = ""
+        for word in desc.split():
+            new_chunk = f"{chunk} {word.strip()}"
+            if len(new_chunk) >= col_width:
+                desc_chunks.append(new_chunk)
+                chunk = ""
+            else:
+                chunk = new_chunk
+
+        delim = "\n"
+
+        return delim.join(desc_chunks)
+
+    def _to_json(self):
+        """Return the entire object in JSON format."""
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=False)
+
+    def to_object(self):
+        """Return the entire object."""
+        return json.loads(self._to_json())
 
 
 def parse_command_line() -> object:
@@ -84,7 +171,21 @@ def parse_command_line() -> object:
         required=False
     )
 
+    parser.add_argument(
+        '-p',
+        '--show_progress',
+        help='Show a progress indicator as data is retrieved.',
+        action="store_true",
+        required=False
+    )
+
     return parser.parse_args()
+
+
+def inform(msg: str):
+    """Provide informational updates to the user as the program progresses."""
+    if PROGRESS:
+        print("  %-80s" % msg, end="\r", flush=True)  # pylint: disable=C0209
 
 
 def get_spotlight_matches(cves: list) -> list:
@@ -109,82 +210,33 @@ def remove_exclusions(resultset: dict) -> dict:
     return resultset
 
 
-def chunk_long_description(desc, col_width) -> str:
-    """Chunks a long string by delimiting with CR based upon column length."""
-    desc_chunks = []
-    chunk = ""
-    for word in desc.split():
-        new_chunk = f"{chunk} {word.strip()}"
-        if len(new_chunk) >= col_width:
-            desc_chunks.append(new_chunk)
-            chunk = ""
-        else:
-            chunk = new_chunk
-
-    delim = "\n"
-
-    return delim.join(desc_chunks)
-
-
 def get_match_details(match_list: list) -> list:
     """Retrieve details for individual matches to the specified CVEs."""
     returned = []
+    inform("[Retrieve CVE matches]")
     match_results = spotlight.get_vulnerabilities(ids=match_list)
     if match_results["status_code"] >= 400:
         raise SystemExit(match_results["body"]["errors"][0]["message"])
 
     for result in match_results["body"]["resources"]:
-        impacted_host_hostname = "Not found"
-        impacted_host_local_ip = "Not found"
-        impacted_host_os_version = "Not found"
-        impacted_host_service_provider = "Not found"
-
-        host_detail = hosts.get_device_details(ids=result["aid"])["body"]["resources"]
-        for host in host_detail:
-            if "hostname" in host:
-                impacted_host_hostname = host["hostname"]
-            if "local_ip" in host:
-                impacted_host_local_ip = host["local_ip"]
-            if "os_version" in host:
-                impacted_host_os_version = host["os_version"]
-            if "service_provider" in host:
-                impacted_host_service_provider = host["service_provider"]
-        try:
-            remediation = chunk_long_description(
-                spotlight.get_remediations_v2(
-                    ids=result["remediation"]["ids"]
-                    )["body"]["resources"][0]["action"],
-                30
-                )
-        except KeyError:
-            remediation = "Not found"
-        if not remediation:
-            remediation = "Not found"
-        description = chunk_long_description(result["cve"]["description"].strip(), 15)
-
-        row = {
-            "cve": result["cve"]["id"],
-            "score": result["cve"]["base_score"],
-            "severity": result["cve"]["severity"],
-            "cve_description": description,
-            "created_on": result["created_timestamp"],
-            "updated_on": result["updated_timestamp"],
-            "hostname": impacted_host_hostname,
-            "local_ip": impacted_host_local_ip,
-            "os_version": impacted_host_os_version,
-            "service_provider": impacted_host_service_provider,
-            "remediation": remediation
-        }
+        row = SpotlightCVEMatch(result).to_object()
+        inform(f"[{row['cve']}] Found {row['hostname']}/{row['local_ip']}")
         returned.append(row)
 
     return returned
 
+
+# Use our module docstring for the banner to our help text
+BANNER = __doc__
+
+# Allow formats for our tabular output
 TABLE_FORMATS = [
     "plain", "simple", "github", "grid", "fancy_grid", "pipe", "orgtbl", "jira", "presto",
     "pretty", "psql", "rst", "mediawiki", "moinmoin", "youtrack", "html", "unsafehtml",
     "latex", "latex_raw", "latex_booktabs", "latex_longtable", "textile", "tsv"
 ]
 
+# Consume inbound command line parameters
 args = parse_command_line()
 BASE = "us1"
 if args.base_url:
@@ -196,6 +248,7 @@ for cve in args.cve.upper().split(","):
         CVE_LIST.append(f"CVE-{cve}")
     else:
         CVE_LIST.append(cve)
+
 EXCLUDE = []
 if args.exclude:
     EXCLUDE = args.exclude.split(",")
@@ -212,6 +265,9 @@ if args.sort:
     if sort_dir in ["asc", "desc"]:
         SORT = sort_dir
 
+PROGRESS = args.show_progress
+
+# Connect to the API and create instances of the SpotlightVulnerabilities and Hosts Service Classes
 auth = OAuth2(client_id=args.client_id,
               client_secret=args.client_secret,
               base_url=BASE
@@ -219,6 +275,7 @@ auth = OAuth2(client_id=args.client_id,
 spotlight = SpotlightVulnerabilities(auth_object=auth)
 hosts = Hosts(auth_object=auth)
 
+# Headers used for our results display
 HEADERS = {
     "cve": "CVE",
     "score": "Score",
@@ -233,7 +290,12 @@ HEADERS = {
     "remediation": "Remediation"
 }
 
+# Run the process
+inform("[Process startup]")
 details = get_match_details(get_spotlight_matches(CVE_LIST))
+
+# Display results
+inform("[Results display]")
 print(
     tabulate(
         tabular_data=remove_exclusions(details),
