8e9aa79bd24f199bdbf24ce06699a96a076a6142
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-03-02 15:01:42 -0500

    Add MSSP functionality

--- samples/hosts/sensor_versions_by_hostname.py
+++ samples/hosts/sensor_versions_by_hostname.py
@@ -22,6 +22,12 @@ def parse_command_line() -> object:
         required=True
         )
     parser.add_argument(
+        '-m',
+        '--mssp',
+        help='Child CID to access (MSSP only)',
+        required=False
+        )
+    parser.add_argument(
         '-b',
         '--base_url',
         help='CrowdStrike API region (us1, us2, eu1, usgov1).'
@@ -42,44 +48,52 @@ def parse_command_line() -> object:
 def device_list(off: int, limit: int, sort: str):
     """Return a list of all devices for the CID, paginating when necessary."""
     result = falcon.query_devices_by_filter(limit=limit, offset=off, sort=sort)
-    new_offset = result["body"]["meta"]["pagination"]["offset"]
-    total = result["body"]["meta"]["pagination"]["total"]
-    returned_device_list = result["body"]["resources"]
-    return new_offset, total, returned_device_list
+    new_offset = 0
+    total = 0
+    returned_device_list = []
+    if result["status_code"] == 200:
+        new_offset = result["body"]["meta"]["pagination"]["offset"]
+        total = result["body"]["meta"]["pagination"]["total"]
+        returned_device_list = result["body"]["resources"]
 
+    return new_offset, total, returned_device_list
 
 def device_detail(aids: list):
     """Return the device_id and agent_version for a list of AIDs provided."""
     result = falcon.get_device_details(ids=aids)
     device_details = []
-    # return just the aid and agent version
-    for device in result["body"]["resources"]:
-        res = {}
-        res["hostname"] = device.get("hostname", None)
-        res["agent_version"] = device.get("agent_version", None)
-        device_details.append(res)
+    if result["status_code"] == 200:
+        # return just the aid and agent version
+        for device in result["body"]["resources"]:
+            res = {}
+            res["hostname"] = device.get("hostname", None)
+            res["agent_version"] = device.get("agent_version", None)
+            device_details.append(res)
     return device_details
 
 
 args = parse_command_line()
 
+BASE = "auto"
 if args.base_url:
     BASE = args.base_url
-else:
-    BASE = "us1"
 
+SORT = "hostname.asc"
 if args.reverse:
     SORT = "hostname.desc"
-else:
-    SORT = "hostname.asc"
+
+CHILD = None
+if args.mssp:
+    CHILD = args.mssp
 
 falcon = Hosts(client_id=args.client_id,
                client_secret=args.client_secret,
-               base_url=BASE
+               base_url=BASE,
+               member_cid=CHILD
                )
 
 OFFSET = 0      # Start at the beginning
-DISPLAYED = 0   # This is just so we can show a running count
+DISPLAYED = 0   # Running count
 TOTAL = 1       # Assume there is at least one
 LIMIT = 500     # Quick limit to prove pagination
 while OFFSET < TOTAL:
@@ -88,3 +102,6 @@ while OFFSET < TOTAL:
     for detail in details:
         DISPLAYED += 1
         print(f"{DISPLAYED}: {detail['hostname']} is on version {detail['agent_version']}")
+
+if not DISPLAYED:
+    print("No results returned.")
