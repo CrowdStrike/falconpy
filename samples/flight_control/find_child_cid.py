0f9bf50aac93801d3bf674235501e79f4b193f32
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-07-19 10:55:15 -0400

    Add AID search functionality

--- samples/flight_control/find_child_cid.py
+++ samples/flight_control/find_child_cid.py
@@ -42,19 +42,22 @@ def parse_command_line() -> object:
         '--base_url',
         help='CrowdStrike API region (us1, us2, eu1, usgov1)'
         ' NOT required unless you are using `usgov1`',
-        required=False
+        required=False,
+        default="auto"
     )
-
+    parser.add_argument(
+        '-f',
+        '--find_host',
+        help='Hostname or Device ID to identify',
+        required=True
+    )
     return parser.parse_args()
 
 
 args = parse_command_line()
-if not args.base_url:
-    BASE = "us1"
-else:
-    BASE = args.base_url
+BASE = args.base_url
 
-HOSTNAME = "HOSTNAME_HERE"
+HOSTNAME = args.find_host
 
 mssp = FlightControl(client_id=args.client_id, client_secret=args.client_secret, base_url=BASE)
 
@@ -72,7 +75,7 @@ if children["status_code"] == 200:
             )
         for auth in AUTH:
             hosts = Hosts(auth_object=auth)
-            lookup = hosts.query_devices_by_filter(filter=f"hostname:'{HOSTNAME}'")
+            lookup = hosts.query_devices_by_filter(filter=f"hostname:'{HOSTNAME}',device_id:'{HOSTNAME}'")
             if lookup["body"]["resources"]:
                 for host in lookup["body"]["resources"]:
                     detail = hosts.get_device_details(ids=host)
