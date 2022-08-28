ec7f2e2b176d0a69393ca6e664625b285c6a047a
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-08-27 20:08:19 -0400

    Default exclude manual testing from main series

--- util/run-tests.sh
+++ util/run-tests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #
-coverage run --rcfile=util/coverage.config -m pytest -s -v
+
+DO_MANUAL="--ignore-glob=**/manual/*"
+if ! [ -z "$1" ]
+then
+  if [ "$1" == "--include-manual" ]
+  then
+    DO_MANUAL=""
+  fi
+fi
+coverage run --rcfile=util/coverage.config -m pytest -s -v $DO_MANUAL
 coverage report
 bandit -r src
