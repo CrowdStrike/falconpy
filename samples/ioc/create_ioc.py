0f110e437e471399ff34b85afb43f9531470f641
Joshua Hiller <joshua.hiller@crowdstrike.com>
2023-01-22 01:24:56 -0500

    Refactor create IOC sample

--- samples/ioc/create_ioc.py
+++ samples/ioc/create_ioc.py
@@ -10,64 +10,82 @@
 Create IOC Example - @jshcodes 06.23.21
 
 FalconPy v.0.8.6+
+
+INDICATOR FILE FORMAT EXAMPLE (JSON)
+{
+    "source": "Test",
+    "action": "detect",
+    "expiration": "2023-01-22T15:00:00.000Z",
+    "description": "Testing",
+    "type": "ipv4",
+    "value": "4.1.42.34",
+    "platforms": ["linux"],
+    "severity": "LOW",
+    "applied_globally": true
+}
 """
 import json
+import os
+from argparse import ArgumentParser, RawTextHelpFormatter
 from falconpy import APIHarness, IOC
 
 
-def connect_api(class_type: str = "service"):
-    """Connect to the selected API service."""
-    with open("../config.json", "r", encoding="utf-8") as cred_file:
-        config = json.loads(cred_file.read())
+def consume_command_line():
+    parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-k", "--falcon_client_id", help="Falcon API Client ID", required=True)
+    parser.add_argument("-s", "--falcon_client_secret", help="Falcon API Client Secret", required=True)
+    parser.add_argument("-m", "--method", help="SDK method to use ('service' or 'uber').", required=False, default="service")
+    parser.add_argument("-i", "--indicator", help="Path to the file representing the indicator (JSON format).", default="example_indicator.json", required=False)
 
+    return parser.parse_args()
+
+
+def connect_api(class_type: str = "service", creds: dict = None):
+    """Connect to the selected API service."""
     if class_type.lower() == "service":
-        falcon_api = IOC(client_id=config["falcon_client_id"],
-                         client_secret=config["falcon_client_secret"]
-                         )
+        falcon_api = IOC(creds=creds)
     elif class_type.lower() == "uber":
-        falcon_api = APIHarness(client_id=config["falcon_client_id"],
-                                client_secret=config["falcon_client_secret"]
-                                )
-    else:
-        falcon_api = None
+        falcon_api = APIHarness(creds=creds)
 
     return falcon_api
 
 
-# Create an IOC using the IOC Service class
-falcon = connect_api("service")
-# Since we are using a version greater than 0.7.3,
-# we are able to make use of Body Payload abstraction
-response = falcon.indicator_create(source="Test",
-                                   action="detect",
-                                   expiration="2021-12-05T05:00:00.000Z",
-                                   description="Testing",
-                                   type="ipv4",
-                                   value="8.7.6.53",
-                                   severity="LOW",
-                                   applied_globally=True,
-                                   platforms="linux"  # Can be a list or a comma-delimited string.
-                                   )
-print(response)
-
-# Create an IOC using the Uber class
-falcon = connect_api("uber")
-# The Uber class does not support Body Payload abstraction.
-# We will send it a standard body payload instead.
-BODY = {
-    "indicators": [
-        {
-            "source": "Test",
-            "action": "detect",
-            "expiration": "2021-12-05T05:00:00.000Z",
-            "description": "Testing",
-            "type": "ipv4",
-            "value": "4.1.2.33",
-            "platforms": ["linux"],
-            "severity": "LOW",
-            "applied_globally": True
-        }
-    ]
+args = consume_command_line()
+credentials = {
+    "client_id": args.falcon_client_id,
+    "client_secret": args.falcon_client_secret
 }
-response = falcon.command('indicator_create_v1', body=BODY)
-print(response)
+if args.method not in ["service", "uber"]:
+    args.method = "service"
+
+falcon = connect_api(args.method, credentials)
+
+if not os.path.exists(args.indicator):
+    raise SystemExit("Unable to load indicator file.")
+
+with open(args.indicator, "r", encoding="utf-8") as indicator:
+    ind_json = json.loads(indicator.read())
+    if args.method == "service":
+        # Create an IOC using the IOC Service class
+        # Since we are using a version greater than 0.7.3,
+        # we are able to make use of Body Payload abstraction
+        response = falcon.indicator_create(source=ind_json["source"],
+                                           action=ind_json["action"],
+                                           expiration=ind_json["expiration"],
+                                           description=ind_json["description"],
+                                           type=ind_json["type"],
+                                           value=ind_json["value"],
+                                           severity=ind_json["severity"],
+                                           applied_globally=ind_json["applied_globally"],
+                                           platforms=ind_json["platforms"]  # Can be a list or a comma-delimited string.
+                                           )
+        print(response)
+    elif args.method == "uber":
+        # Create an IOC using the Uber class
+        # The Uber class does not support Body Payload abstraction.
+        # We will send it a standard body payload instead.
+        BODY = {
+            "indicators": [ind_json]
+        }
+        response = falcon.command('indicator_create_v1', body=BODY)
+        print(response)
