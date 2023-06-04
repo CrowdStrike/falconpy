f7782c3ef3e096ff5fb743e22f23f33a03f8a8d9
Joshua Hiller <joshua.hiller@crowdstrike.com>
2023-06-04 17:34:36 -0400

    Add / rename D4CRegistration operations

--- src/falconpy/_endpoint/_d4c_registration.py
+++ src/falconpy/_endpoint/_d4c_registration.py
@@ -190,7 +190,7 @@ _d4c_registration_endpoints = [
     ]
   ],
   [
-    "GetCSPMAzureAccount",
+    "GetDiscoverCloudAzureAccount",
     "GET",
     "/cloud-connect-azure/entities/account/v1",
     "Return information about Azure account registration",
@@ -199,15 +199,22 @@ _d4c_registration_endpoints = [
       {
         "type": "array",
         "items": {
-          "maxLength": 36,
-          "minLength": 36,
-          "pattern": "^[0-9a-z-]{36}$",
           "type": "string"
         },
         "collectionFormat": "multi",
         "description": "SubscriptionIDs of accounts to select for this status operation. "
         "If this is empty then all accounts are returned.",
         "name": "ids",
+        "in": "query"
+      },
+      {
+        "type": "array",
+        "items": {
+          "type": "string"
+        },
+        "collectionFormat": "multi",
+        "description": "Tenant ids to filter azure accounts",
+        "name": "tenant_ids",
         "in": "query"
       },
       {
@@ -218,11 +225,33 @@ _d4c_registration_endpoints = [
         "description": "Type of scan, dry or full, to perform on selected accounts",
         "name": "scan-type",
         "in": "query"
+      },
+      {
+        "pattern": "^(provisioned|operational)$",
+        "type": "string",
+        "description": "Account status to filter results by.",
+        "name": "status",
+        "in": "query"
+      },
+      {
+        "maxLength": 3,
+        "minLength": 1,
+        "type": "integer",
+        "default": 100,
+        "description": "The maximum records to return. Defaults to 100.",
+        "name": "limit",
+        "in": "query"
+      },
+      {
+        "type": "integer",
+        "description": "The offset to start retrieving records from",
+        "name": "offset",
+        "in": "query"
       }
     ]
   ],
   [
-    "CreateCSPMAzureAccount",
+    "CreateDiscoverCloudAzureAccount",
     "POST",
     "/cloud-connect-azure/entities/account/v1",
     "Creates a new account in our system for a customer and generates a script for them to run "
@@ -237,7 +266,7 @@ _d4c_registration_endpoints = [
     ]
   ],
   [
-    "UpdateCSPMAzureAccountClientID",
+    "UpdateDiscoverCloudAzureAccountClientID",
     "PATCH",
     "/cloud-connect-azure/entities/client-id/v1",
     "Update an Azure service account in our system by with the user-created client_id "
@@ -253,6 +282,24 @@ _d4c_registration_endpoints = [
         "name": "id",
         "in": "query",
         "required": True
+      },
+      {
+        "maxLength": 36,
+        "minLength": 36,
+        "pattern": "^[0-9a-z-]{36}$",
+        "type": "string",
+        "description": "Object ID to use for the Service Principal associated with the customer's Azure account",
+        "name": "object_id",
+        "in": "query"
+      },
+      {
+        "maxLength": 36,
+        "minLength": 36,
+        "pattern": "^[0-9a-z-]{36}$",
+        "type": "string",
+        "description": "Tenant ID to update client ID for. Required if multiple tenants are registered.",
+        "name": "tenant-id",
+        "in": "query"
       }
     ]
   ],
@@ -278,31 +325,70 @@ _d4c_registration_endpoints = [
         "required": True
       },
       {
-        "maxLength": 5,
-        "minLength": 4,
-        "pattern": "^(true|false)$",
-        "enum": [
-          "false",
-          "true"
-        ],
-        "type": "string",
+        "type": "boolean",
         "default": False,
+        "description": "Setting to true will invalidate the current certificate and generate a new certificate",
         "name": "refresh",
+        "in": "query"
+      },
+      {
+        "maxLength": 2,
+        "minLength": 1,
+        "pattern": "^[0-9]{1,2}$",
+        "type": "string",
+        "description": "Years the certificate should be valid (only used when refresh=true)",
+        "name": "years_valid",
         "in": "query"
       }
     ]
   ],
   [
-    "GetCSPMAzureUserScriptsAttachment",
+    "GetDiscoverCloudAzureTenantIDs",
+    "GET",
+    "/cloud-connect-azure/entities/tenant-id/v1",
+    "Return available tenant ids for discover for cloud",
+    "d4c_registration",
+    []
+  ],
+  [
+    "GetDiscoverCloudAzureUserScriptsAttachment",
     "GET",
     "/cloud-connect-azure/entities/user-scripts-download/v1",
     "Return a script for customer to run in their cloud environment to grant us access to their "
     "Azure environment as a downloadable attachment",
     "d4c_registration",
-    []
+    [
+      {
+        "type": "array",
+        "items": {
+          "type": "string"
+        },
+        "collectionFormat": "csv",
+        "description": "Azure Tenant ID",
+        "name": "tenant-id",
+        "in": "query",
+        "required": True
+      },
+      {
+        "type": "array",
+        "items": {
+          "type": "string"
+        },
+        "collectionFormat": "csv",
+        "description": "Azure Subscription ID",
+        "name": "subscription_ids",
+        "in": "query"
+      },
+      {
+        "type": "string",
+        "description": "Template to be rendered",
+        "name": "template",
+        "in": "query"
+      }
+    ]
   ],
   [
-    "GetCSPMAzureUserScripts",
+    "GetDiscoverCloudAzureUserScripts",
     "GET",
     "/cloud-connect-azure/entities/user-scripts/v1",
     "Return a script for customer to run in their cloud environment to grant us access to their "
@@ -311,36 +397,78 @@ _d4c_registration_endpoints = [
     []
   ],
   [
-    "GetCSPMCGPAccount",
+    "GetD4CCGPAccount",
     "GET",
     "/cloud-connect-gcp/entities/account/v1",
     "Returns information about the current status of an GCP account.",
     "d4c_registration",
     [
       {
-        "maxLength": 4,
-        "minLength": 3,
-        "pattern": "^(full|dry)$",
+        "enum": [
+          "Folder",
+          "Organization",
+          "Project"
+        ],
+        "type": "string",
+        "description": "GCP Hierarchy Parent Type, organization/folder/project",
+        "name": "parent_type",
+        "in": "query"
+      },
+      {
+        "type": "array",
+        "items": {
+          "type": "string"
+        },
+        "collectionFormat": "multi",
+        "description": "Hierarchical Resource IDs of accounts",
+        "name": "ids",
+        "in": "query"
+      },
+      {
+        "enum": [
+          "dry",
+          "full"
+        ],
         "type": "string",
         "description": "Type of scan, dry or full, to perform on selected accounts",
         "name": "scan-type",
         "in": "query"
       },
       {
-        "type": "array",
-        "items": {
-          "pattern": "\\d{10,}",
-          "type": "string"
-        },
-        "collectionFormat": "multi",
-        "description": "Parent IDs of accounts",
-        "name": "ids",
+        "enum": [
+          "operational",
+          "provisioned"
+        ],
+        "type": "string",
+        "description": "Account status to filter results by.",
+        "name": "status",
+        "in": "query"
+      },
+      {
+        "maxLength": 3,
+        "minLength": 1,
+        "type": "integer",
+        "default": 100,
+        "description": "The maximum records to return. Defaults to 100.",
+        "name": "limit",
+        "in": "query"
+      },
+      {
+        "type": "integer",
+        "description": "The offset to start retrieving records from",
+        "name": "offset",
+        "in": "query"
+      },
+      {
+        "type": "string",
+        "description": "Order fields in ascending or descending order. Ex: parent_type|asc.",
+        "name": "sort",
         "in": "query"
       }
     ]
   ],
   [
-    "CreateCSPMGCPAccount",
+    "CreateD4CGCPAccount",
     "POST",
     "/cloud-connect-gcp/entities/account/v1",
     "Creates a new account in our system for a customer and generates a new service account for them "
@@ -364,13 +492,25 @@ _d4c_registration_endpoints = [
     []
   ],
   [
-    "GetCSPMGCPUserScripts",
+    "GetD4CGCPUserScripts",
     "GET",
     "/cloud-connect-gcp/entities/user-scripts/v1",
     "Return a script for customer to run in their cloud environment to grant us access to their "
     "GCP environment",
     "d4c_registration",
-    []
+    [
+      {
+        "enum": [
+          "Folder",
+          "Organization",
+          "Project"
+        ],
+        "type": "string",
+        "description": "GCP Hierarchy Parent Type, organization/folder/project",
+        "name": "parent_type",
+        "in": "query"
+      }
+    ]
   ],
   [
     "GetHorizonD4CScripts",
