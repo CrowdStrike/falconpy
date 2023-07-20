f7324fdf35c4aa973cfd2e43ae2eeded05480b27
Russell Snyder <russ@arceo.ai>
2023-07-19 20:14:13 -0400

    Fixed dependency issue in create-lambda-layer.sh script

--- util/create-lambda-layer.sh
+++ util/create-lambda-layer.sh
@@ -7,11 +7,14 @@
 # Assumes the docker service is running.
 #
 # Currently targets Python 3.8
-echo "crowdstrike-falconpy" > lambda-requirements.txt
+cat - << EOF > lambda-requirements.txt
+crowdstrike-falconpy
+urllib3 < 2.0
+EOF
 # Remove any old copies
 rm falconpy-layer.zip >/dev/null 2>&1
 # Run the lambci image and install the requirements
-docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8 \
+docker run --rm --entrypoint '' -v $(pwd):/foo -w /foo public.ecr.aws/lambda/python:3.8 \
 pip install -r lambda-requirements.txt -t python
 # Create the layer archive
 zip -r falconpy-layer.zip python
