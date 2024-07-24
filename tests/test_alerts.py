7d86f3f87bdb69dcaf2e6220f14366754aed1c44
Joshua Hiller <joshua.hiller@crowdstrike.com>
2024-07-24 01:07:35 -0400

    Move generic attribute tests

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -2,6 +2,7 @@
 # This class tests the Alerts service class
 import os
 import sys
+import pytest
 
 # Authentication via the test_authorization.py
 from tests import test_authorization as Authorization
@@ -9,6 +10,7 @@ from tests import test_authorization as Authorization
 sys.path.append(os.path.abspath('src'))
 # Classes to test - manually imported from sibling folder
 from falconpy import Alerts
+from falconpy._util import service_request
 
 auth = Authorization.TestAuthorization()
 config = auth.getConfigObject()
@@ -104,3 +106,30 @@ class TestAlerts:
 
     def test_all_functionality(self):
         assert self.alerts_run_all_tests() is True
+
+    def test_token_refresh(self):
+        falconWithObject = Alerts(auth_object=config)
+        check = falconWithObject.auth_object.token()
+        if check["status_code"] == 429:
+            pytest.skip("Rate limit hit")
+
+        if not falconWithObject.token_expired():
+            falconWithObject.auth_object.token_expiration = 0  # Forcibly expire the current token
+            if falconWithObject.query_alerts_v2(parameters={"limit": 1})["status_code"] in AllowedResponses:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def test_force_attribute_error(self):
+        FULL_URL = falcon.base_url+'/alerts/queries/alerts/v2'
+        if service_request(caller=self,
+                           method="GET",
+                           endpoint=FULL_URL,
+                           headers=falcon.headers,
+                           verify=falcon.ssl_verify
+                           )["status_code"] in AllowedResponses:
+            return True
+        else:
+            return False
