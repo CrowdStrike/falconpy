6b90765fb6853c97413d44bd9eb40868cf36ab47
Joshua Hiller <joshua.hiller@crowdstrike.com>
2024-07-24 01:08:25 -0400

    Unit test behavior update

--- tests/test_container_detections.py
+++ tests/test_container_detections.py
@@ -34,6 +34,6 @@ class TestContainerDetections:
         for key in tests:
             if tests[key]["status_code"] not in AllowedResponses:
                 error_checks = False
-                print(key)
-                print(tests[key])
+                # print(key)
+                # print(tests[key])
         assert error_checks
