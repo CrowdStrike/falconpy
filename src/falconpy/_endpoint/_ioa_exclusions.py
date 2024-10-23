ae3f69819ea35630097b24410cb0b19a2383b5ba
Joshua Hiller <joshua.hiller@crowdstrike.com>
2024-10-22 22:52:27 -0400

    Add cl_regex and ifn_regex to the queryIOAExclusionsV1 operation

--- src/falconpy/_endpoint/_ioa_exclusions.py
+++ src/falconpy/_endpoint/_ioa_exclusions.py
@@ -120,8 +120,23 @@ _ioa_exclusions_endpoints = [
     [
       {
         "type": "string",
-        "description": "The filter expression that should be used to limit the results.",
+        "description": "The filter expression that should be used to limit the results. Filtered queries "
+        "involving regex fields should specify their expressions in the `ifn_regex` and `cl_regex` parameters.",
         "name": "filter",
+        "in": "query"
+      },
+      {
+        "type": "string",
+        "description": "The `ifn_regex` expression to filter exclusions by, used alongside expressions "
+        "specified in the filter query parameter.",
+        "name": "ifn_regex",
+        "in": "query"
+      },
+      {
+        "type": "string",
+        "description": "The `cl_regex` expression to filter exclusions by, used alongside expressions "
+        "specified in the filter query parameter.",
+        "name": "cl_regex",
         "in": "query"
       },
       {
