0f04be960c07dfdab6e8515be037ed744525254f
Joshua Hiller <joshua.hiller@crowdstrike.com>
2024-10-24 02:11:39 -0400

    Add new Downloads service collection

---
+++ tests/test_downloads.py
@@ -0,0 +1,33 @@
+# test_downloads.py
+# This class tests the Downloads service class
+
+# import json
+import os
+import sys
+
+# Authentication via the test_authorization.py
+from tests import test_authorization as Authorization
+
+# Import our sibling src folder into the path
+sys.path.append(os.path.abspath('src'))
+# Classes to test - manually imported from sibling folder
+from falconpy import Downloads
+
+auth = Authorization.TestAuthorization()
+config = auth.getConfigObject()
+falcon = Downloads(auth_object=config)
+AllowedResponses = [200, 201, 207, 404, 429]
+
+class TestDownloads:
+    def test_all_code_paths(self):
+        error_checks = True
+        tests = {
+            "DownloadFile": falcon.download(file_name="the_file_named_jeff.txt", file_version="1"),
+            "EnumerateFile": falcon.enumerate()
+        }
+        for key in tests:
+            if tests[key]["status_code"] not in AllowedResponses:
+                error_checks = False
+                # print(key)
+                # print(tests[key])
+        assert error_checks
