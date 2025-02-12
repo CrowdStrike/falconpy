fdf775b1607834d92ea50a34f338e65d796e446f
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-02-11 19:41:44 -0500

    Bump version -> 1.4.7

--- src/falconpy/_version.py
+++ src/falconpy/_version.py
@@ -35,7 +35,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <https://unlicense.org>
 """
-_VERSION = '1.4.6'
+_VERSION = '1.4.7'
 _MAINTAINER = 'Joshua Hiller'
 _AUTHOR = 'CrowdStrike'
 _AUTHOR_EMAIL = 'falconpy@crowdstrike.com'
