7c20d5e19be8a5511e02a4e7e4ce77b77d77a5d4
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-03-10 11:20:51 -0400

    NGSIEM payload handling fixes. Relates to #1291.

--- tests/test_ngsiem.py
+++ tests/test_ngsiem.py
@@ -21,7 +21,7 @@ AllowedResponses = [200, 201, 400, 403, 404, 429]  # Temp allow 403
 class TestNGSIEM:
     def run_all_tests(self):
         test_search = {
-            "showQueryEventDistribution" : True,
+            #"showQueryEventDistribution" : True,
             "isLive" : False,
             "start" : "1d",
             "queryString" : "#event_simpleName=*"
@@ -36,42 +36,54 @@ class TestNGSIEM:
                                                                                                package="moe",
                                                                                                namespace="jack"
                                                                                                ),
-            "GetLookupFromPackageWithNamespaceV1Fail": falcon.get_file_from_package_with_namespace(filename="manny",
-                                                                                                   package="moe",
-                                                                                                   namespace="jack"
-                                                                                                   ),
             "GetLookupFromPackageV1": falcon.get_file_from_package(repository="search-all",
                                                                    filename="manny",
                                                                    package="moe"
                                                                    ),
-            "StartSearchV1": falcon.start_search(repository="search-all", search=test_search),
+            "StartSearchV1": falcon.start_search(repository="search-all", search=test_search, is_live=False, start="1d", timezone_offset_minutes=0),
+            "StartSearchV1B": falcon.start_search(repository="search-all", query_string="#event_simpleName=*", is_live=False, start="1d", timezone_offset_minutes=0),
         }
         for key in tests:
             if tests[key]["status_code"] not in AllowedResponses:
                 error_checks = False
-                # if not error_checks:
-                #     print(tests[key])
             else:
                 if key == "StartSearchV1":
                     search_id = tests[key]["resources"].get("id", None)
 
         follow_up_tests = {
             "GetSearchStatusV1": falcon.get_search_status(repository="search-all", search_id=search_id),
+            "GetSearchStatusV1": falcon.get_search_status(repository="search-all", id=search_id),
             "StopSearchV1": falcon.stop_search(repository="search-all", search_id=search_id)
         }
         for follow_key in follow_up_tests:
             if follow_up_tests[follow_key]["status_code"] not in AllowedResponses:
                 error_checks = False
 
+        fail_tests = {
+            "UploadLookupV1-fail": falcon.upload_file(repository="search-all", lookup_file="tests/badfile.csv"),
+            "UploadLookupV1-fail2": falcon.upload_file(repository="search-all"),
+            "GetLookupV1-fail": falcon.get_file(),
+            "GetLookupFromPackageWithNamespaceV1Fail": falcon.get_file_from_package_with_namespace(),
+            "GetLookupFromPackageV1-fail": falcon.get_file_from_package(filename="manny",
+                                                                        package="moe"
+                                                                        ),
+            "StartSearchV1-fail": falcon.start_search(search=test_search, is_live=False, start="1d", timezone_offset_minutes=0),
+            "GetSearchStatusV1-fail": falcon.get_search_status(repository="search-all"),
+            "StopSearchV1-fail": falcon.stop_search(repository="search-all")
+        }
+        for key in fail_tests:
+            if fail_tests[key]["status_code"] != 500:
+                error_checks = False
         # Test lookup file download
-        try:
-            binary_download_test = falcon.get_file(repository="search-all", filename="testfile.csv", expand_result=True)[0]
-        except Exception:
-            pytest.skip("Skipping on failure")
+        # try:
+        # Expanding result so we can retrieve the status code from a binary response
+        binary_download_test = falcon.get_file(repository="search-all", filename="testfile.csv", expand_result=True)[0]
+        # except Exception:
+        #     pytest.skip("Skipping on failure")
         if binary_download_test not in AllowedResponses:
             error_checks = False
-        if not error_checks:
-            pytest.skip("Skipping on failure")  # Skip on failure for now
+        # if not error_checks:
+        #     pytest.skip("Skipping on failure")  # Skip on failure for now
         return error_checks
 
     def test_all_functionality(self):
