4efe3ce375d57e5a38fa088fa87b7394beb683f7
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-04-23 17:01:01 -0400

    Allow 404 when policy ID is not found in ContentUpdatePolicies unit testing.

--- tests/test_content_update_policies.py
+++ tests/test_content_update_policies.py
@@ -16,7 +16,7 @@ from falconpy import ContentUpdatePolicies
 auth = Authorization.TestAuthorization()
 config = auth.getConfigObject()
 falcon = ContentUpdatePolicies(auth_object=config)
-AllowedResponses = [200, 201, 207, 400, 403, 429]
+AllowedResponses = [200, 201, 207, 400, 403, 404, 429]
 
 
 class TestContentUpdatePolicies:
