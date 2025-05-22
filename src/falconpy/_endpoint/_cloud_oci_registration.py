e04fda85e8d78235be3c1538997080cdac13be1a
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-05-22 01:43:38 -0400

    Cosmetic updates to the filter parameter for the cloud_security_registration_oci_get_account operation

--- src/falconpy/_endpoint/_cloud_oci_registration.py
+++ src/falconpy/_endpoint/_cloud_oci_registration.py
@@ -47,7 +47,7 @@ _cloud_oci_registration_endpoints = [
       {
         "type": "string",
         "description": "FQL (Falcon Query Language) string for filtering results. Allowed filters are "
-        "Set{updated_at, tenancy_ocid, tenancy_name, home_region, key_age, overall_status, created_at}",
+        "Set{overall_status, created_at, updated_at, tenancy_ocid, tenancy_name, home_region, key_age}",
         "name": "filter",
         "in": "query"
       },
