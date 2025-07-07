5009d218cbaf150c9bb00fe9bc2e92c674d6241f
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-07-07 01:21:51 -0400

    Add tag_key and tag_value as allowed filter values to cloud_security_assets_combined_compliance_by_account

--- src/falconpy/_endpoint/_cloud_security_assets.py
+++ src/falconpy/_endpoint/_cloud_security_assets.py
@@ -50,7 +50,7 @@ _cloud_security_assets_endpoints = [
         "account_name  assessment_id  business_impact  cloud_group  cloud_label  cloud_label_id  cloud_provider  "
         "cloud_scope  compliant  control.benchmark.name  control.benchmark.version  control.framework  control.name  "
         "control.type  control.version  environment  last_evaluated  region  resource_provider  resource_type  "
-        "resource_type_name  service  service_category  severities",
+        "resource_type_name  service  service_category  severities  tag_key  tag_value",
         "name": "filter",
         "in": "query"
       },
