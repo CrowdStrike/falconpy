cc6cb37443f7aa7276268543b60269d45dc3ac4e
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-08-07 02:24:55 -0400

    Add Cloud Security Compliance service collection

--- src/falconpy/_endpoint/__init__.py
+++ src/falconpy/_endpoint/__init__.py
@@ -24,6 +24,7 @@ from .deprecated import _cloud_aws_registration_deprecated
 from .deprecated import _cloud_azure_registration_deprecated
 from .deprecated import _cloud_oci_registration_deprecated
 from .deprecated import _cloud_security_assets_deprecated
+from .deprecated import _cloud_security_compliance_deprecated
 from .deprecated import _correlation_rules_deprecated
 from .deprecated import _custom_ioa_deprecated
 from .deprecated import _d4c_registration_deprecated
@@ -56,6 +57,7 @@ from ._cloud_aws_registration import _cloud_aws_registration_endpoints
 from ._cloud_azure_registration import _cloud_azure_registration_endpoints
 from ._cloud_oci_registration import _cloud_oci_registration_endpoints
 from ._cloud_security_assets import _cloud_security_assets_endpoints
+from ._cloud_security_compliance import _cloud_security_compliance_endpoints
 from ._cloud_connect_aws import _cloud_connect_aws_endpoints
 from ._cloud_snapshots import _cloud_snapshots_endpoints
 from ._container_image_compliance import _container_image_compliance_endpoints
@@ -152,6 +154,7 @@ api_endpoints.extend(_cloud_aws_registration_endpoints)
 api_endpoints.extend(_cloud_azure_registration_endpoints)
 api_endpoints.extend(_cloud_oci_registration_endpoints)
 api_endpoints.extend(_cloud_security_assets_endpoints)
+api_endpoints.extend(_cloud_security_compliance_endpoints)
 api_endpoints.extend(_cloud_snapshots_endpoints)
 api_endpoints.extend(_container_image_compliance_endpoints)
 api_endpoints.extend(_configuration_assessment_evaluation_logic_endpoints)
@@ -242,6 +245,7 @@ deprecated_endpoints.extend(_cloud_aws_registration_deprecated)
 deprecated_endpoints.extend(_cloud_azure_registration_deprecated)
 deprecated_endpoints.extend(_cloud_oci_registration_deprecated)
 deprecated_endpoints.extend(_cloud_security_assets_deprecated)
+deprecated_endpoints.extend(_cloud_security_compliance_deprecated)
 deprecated_endpoints.extend(_correlation_rules_deprecated)
 deprecated_endpoints.extend(_certificate_based_exclusions_deprecated)
 deprecated_endpoints.extend(_custom_ioa_deprecated)
