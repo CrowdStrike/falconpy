bb5e839795a1fda5f0d3f7a1b36352f7e9e7142d
Joshua Hiller <joshua.hiller@crowdstrike.com>
2025-11-16 02:39:06 -0500

    Add summary as an allowed value for the filter argument in the QueryIntelReportEntities operation within the Intel service collection

--- src/falconpy/_endpoint/_intel.py
+++ src/falconpy/_endpoint/_intel.py
@@ -235,8 +235,8 @@ _intel_endpoints = [
         "include:\n\nactors, actors.animal_classifier, actors.id, actors.name, actors.slug, actors.url, created_date, "
         "description, id, last_modified_date, malware, malware.community_identifiers, malware.family_name, "
         "malware.slug, motivations, motivations.id, motivations.slug, motivations.value, name, name.raw, "
-        "short_description, slug, sub_type, sub_type.id, sub_type.name, sub_type.slug, tags, tags.id, tags.slug, "
-        "tags.value, target_countries, target_countries.id, target_countries.slug, target_countries.value, "
+        "short_description, slug, sub_type, sub_type.id, sub_type.name, sub_type.slug, summary, tags, tags.id, "
+        "tags.slug, tags.value, target_countries, target_countries.id, target_countries.slug, target_countries.value, "
         "target_industries, target_industries.id, target_industries.slug, target_industries.value, type, type.id, "
         "type.name, type.slug, url.",
         "name": "filter",
@@ -771,8 +771,8 @@ _intel_endpoints = [
         "include:\n\nactors, actors.animal_classifier, actors.id, actors.name, actors.slug, actors.url, created_date, "
         "description, id, last_modified_date, malware, malware.community_identifiers, malware.family_name, "
         "malware.slug, motivations, motivations.id, motivations.slug, motivations.value, name, name.raw, "
-        "short_description, slug, sub_type, sub_type.id, sub_type.name, sub_type.slug, tags, tags.id, tags.slug, "
-        "tags.value, target_countries, target_countries.id, target_countries.slug, target_countries.value, "
+        "short_description, slug, sub_type, sub_type.id, sub_type.name, sub_type.slug, summary, tags, tags.id, "
+        "tags.slug, tags.value, target_countries, target_countries.id, target_countries.slug, target_countries.value, "
         "target_industries, target_industries.id, target_industries.slug, target_industries.value, type, type.id, "
         "type.name, type.slug, url.",
         "name": "filter",
