fbc20669a8c8c28f47b3130f1e611e6b92166b03
alhumaw <alhumaw47@gmail.com>
2026-02-04 14:23:07 -0800

    Update Host Migration Service Collection

--- src/falconpy/_endpoint/_host_migration.py
+++ src/falconpy/_endpoint/_host_migration.py
@@ -215,39 +215,39 @@ _host_migration_endpoints = [
       },
       {
         "enum": [
-          "groups|asc",
-          "groups|desc",
-          "groups",
-          "hostname|asc",
-          "hostname|desc",
-          "hostname",
-          "status|asc",
-          "status|desc",
-          "status",
-          "created_time|asc",
-          "created_time|desc",
-          "created_time",
-          "host_migration_id|asc",
-          "host_migration_id|desc",
-          "host_migration_id",
           "hostgroups|asc",
           "hostgroups|desc",
           "hostgroups",
           "static_host_groups|asc",
           "static_host_groups|desc",
           "static_host_groups",
-          "target_cid|asc",
-          "target_cid|desc",
-          "target_cid",
+          "status|asc",
+          "status|desc",
+          "status",
           "source_cid|asc",
           "source_cid|desc",
           "source_cid",
+          "id|asc",
+          "id|desc",
+          "id",
+          "groups|asc",
+          "groups|desc",
+          "groups",
+          "hostname|asc",
+          "hostname|desc",
+          "hostname",
+          "target_cid|asc",
+          "target_cid|desc",
+          "target_cid",
           "migration_id|asc",
           "migration_id|desc",
           "migration_id",
-          "id|asc",
-          "id|desc",
-          "id"
+          "created_time|asc",
+          "created_time|desc",
+          "created_time",
+          "host_migration_id|asc",
+          "host_migration_id|desc",
+          "host_migration_id"
         ],
         "type": "string",
         "description": "The property to sort by.",
@@ -256,9 +256,9 @@ _host_migration_endpoints = [
       },
       {
         "type": "string",
-        "description": "The filter expression that should be used to limit the results. Valid fields: "
-        "created_time, host_migration_id, hostgroups, static_host_groups, target_cid, source_cid, migration_id, id, "
-        "groups, hostname, status",
+        "description": "The filter expression that should be used to limit the results. Valid fields: groups, "
+        "hostname, target_cid, migration_id, created_time, host_migration_id, hostgroups, static_host_groups, status, "
+        "source_cid, id",
         "name": "filter",
         "in": "query"
       }
@@ -285,6 +285,9 @@ _host_migration_endpoints = [
       },
       {
         "enum": [
+          "target_cid|asc",
+          "target_cid|desc",
+          "target_cid",
           "status|asc",
           "status|desc",
           "status",
@@ -305,10 +308,7 @@ _host_migration_endpoints = [
           "id",
           "migration_id|asc",
           "migration_id|desc",
-          "migration_id",
-          "target_cid|asc",
-          "target_cid|desc",
-          "target_cid"
+          "migration_id"
         ],
         "type": "string",
         "description": "The property to sort by.",
@@ -317,8 +317,8 @@ _host_migration_endpoints = [
       },
       {
         "type": "string",
-        "description": "The filter expression that should be used to limit the results. Valid fields: name, "
-        "id, migration_id, target_cid, status, migration_status, created_by, created_time",
+        "description": "The filter expression that should be used to limit the results. Valid fields: "
+        "migration_id, target_cid, status, migration_status, created_by, created_time, name, id",
         "name": "filter",
         "in": "query"
       }
