1a2939add706c64a08c7bcacf62c182ab269794b
alhumaw <alhumaw47@gmail.com>
2026-04-08 09:42:21 -0700

    Update NetworkScanTemplates endpoint definitions

--- src/falconpy/_endpoint/_network_scan_templates.py
+++ src/falconpy/_endpoint/_network_scan_templates.py
@@ -81,21 +81,6 @@ _network_scan_templates_endpoints = [
     ]
   ],
   [
-    "update_templates",
-    "PATCH",
-    "/netscan/entities/templates/v1",
-    "Update \"templates\" using provided specifications",
-    "network_scan_templates",
-    [
-      {
-        "description": "\"templates\" specifications for updating",
-        "name": "body",
-        "in": "body",
-        "required": True
-      }
-    ]
-  ],
-  [
     "delete_templates",
     "DELETE",
     "/netscan/entities/templates/v1",
@@ -116,6 +101,21 @@ _network_scan_templates_endpoints = [
     ]
   ],
   [
+    "update_templates",
+    "PATCH",
+    "/netscan/entities/templates/v1",
+    "Update \"templates\" using provided specifications",
+    "network_scan_templates",
+    [
+      {
+        "description": "\"templates\" specifications for updating",
+        "name": "body",
+        "in": "body",
+        "required": True
+      }
+    ]
+  ],
+  [
     "query_templates",
     "GET",
     "/netscan/queries/templates/v1",
@@ -123,18 +123,18 @@ _network_scan_templates_endpoints = [
     "network_scan_templates",
     [
       {
-        "minimum": 0,
         "type": "integer",
-        "description": "An offset used with the `limit` parameter to manage pagination of results. On your "
-        "first request, don’t provide an `offset`. On subsequent requests, add previous `offset` with the previous "
-        "`limit` to continue from that place in the results",
+        "minimum": 0,
+        "description": "An offset used with the limit parameter to manage pagination of results. On your first "
+        " request, don’t provide an offset. On subsequent requests, add previous offset with the previous limit to "
+        "continue from that place in the results",
         "name": "offset",
         "in": "query"
       },
       {
+        "type": "integer",
         "maximum": 100,
         "minimum": 1,
-        "type": "integer",
         "description": "The number of \"templates IDs\" to return in this response (Min: 1, Max: 100, Default: 100)",
         "name": "limit",
         "in": "query"
