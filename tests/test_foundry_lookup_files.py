0373c62fb32a8cc7896e63424670b7b92a2b02ba
alhumaw <alhumaw47@gmail.com>
2026-06-03 14:49:40 -0700

    Add 401 to AllowedResponses in test_foundry_lookup_files for file upload endpoints

--- tests/test_foundry_lookup_files.py
+++ tests/test_foundry_lookup_files.py
@@ -12,7 +12,7 @@ from falconpy import FoundryLookupFiles
 auth = Authorization.TestAuthorization()
 config = auth.getConfigObject()
 falcon = FoundryLookupFiles(auth_object=config)
-AllowedResponses = [200, 201, 207, 400, 403, 404, 429, 500]
+AllowedResponses = [200, 201, 207, 400, 401, 403, 404, 429, 500]
 
 
 class TestFoundryLookupFiles:
