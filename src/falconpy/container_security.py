a27257129189d77181ef10af620e07b2dd3c2107
alhumaw <alhumaw47@gmail.com>
2026-09-16 11:35:30 -0700

    Add new ContainerSecurity parent service collection

---
+++ src/falconpy/container_security.py
@@ -0,0 +1,82 @@
+"""CrowdStrike Falcon ContainerSecurity API interface class.
+
+ _______                        __ _______ __        __ __
+|   _   .----.-----.--.--.--.--|  |   _   |  |_.----|__|  |--.-----.
+|.  1___|   _|  _  |  |  |  |  _  |   1___|   _|   _|  |    <|  -__|
+|.  |___|__| |_____|________|_____|____   |____|__| |__|__|__|_____|
+|:  1   |                         |:  1   |
+|::.. . |   CROWDSTRIKE FALCON    |::.. . |    FalconPy
+`-------'                         `-------'
+
+OAuth2 API - Customer SDK
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+"""
+
+from .container_alerts import ContainerAlerts
+from .container_detections import ContainerDetections
+from .container_image_compliance import ContainerImageCompliance
+from .container_images import ContainerImages
+from .container_vulnerabilities import ContainerVulnerabilities
+from .drift_indicators import DriftIndicators
+from .image_assessment_policies import ImageAssessmentPolicies
+
+
+class ContainerSecurity(ContainerAlerts,  # pylint: disable=too-many-ancestors
+                        ContainerDetections,
+                        ContainerImageCompliance,
+                        ContainerImages,
+                        ContainerVulnerabilities,
+                        DriftIndicators,
+                        ImageAssessmentPolicies):
+    """Combined ContainerSecurity service collection providing access to all container security operations.
+
+    This class aggregates all ContainerSecurity sub-service collections into a single interface
+    via multiple inheritance. Operations are provided by the following sub-services:
+
+    - ContainerAlerts
+    - ContainerDetections
+    - ContainerImageCompliance
+    - ContainerImages
+    - ContainerVulnerabilities
+    - DriftIndicators
+    - ImageAssessmentPolicies
+
+    The only requirement to instantiate an instance of this class is one of the following.
+
+    - a valid client_id and client_secret provided as keywords.
+    - a credential dictionary with client_id and client_secret containing valid API credentials
+      {
+          "client_id": "CLIENT_ID_HERE",
+          "client_secret": "CLIENT_SECRET_HERE"
+      }
+    - a previously-authenticated instance of the authentication service class (oauth2.py)
+    - a valid token provided by the authentication service class (oauth2.py)
+    """
+
+
+# Backward compatibility alias
+Container_Security = ContainerSecurity  # pylint: disable=C0103
