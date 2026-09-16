d246de00a94ac5e78d6edb11164567095b189cae
alhumaw <alhumaw47@gmail.com>
2026-09-16 14:18:30 -0700

    Update CloudGoogleCloudRegistration: add 1 new operation

--- tests/test_cloud_google_cloud_registration.py
+++ tests/test_cloud_google_cloud_registration.py
@@ -74,6 +74,7 @@ class TestCloudGoogleCloudRegistration:
             "cloud_registration_gcp_delete_registration": falcon.delete_registration(ids="12345678"),
             "cloud_registration_gcp_get_entities": falcon.get_entities(ids=["string"]),
             "cloud_registration_gcp_post_terraform_script": falcon.cloud_registration_gcp_post_terraform_script(resources="string"),
+            "cloud_registration_gcp_post_infra_manager_script": falcon.generate_gcp_infra_manager_script(resources="string"),
         }
         for key in tests:
             if tests[key]["status_code"] not in AllowedResponses:
