5d29a9dc8d601dc9e06f7ba523df08ad0c785aa1
alhumaw <alhumaw47@gmail.com>
2026-09-16 14:18:29 -0700

    Add new Agents service collection

--- src/falconpy/_endpoint/__init__.py
+++ src/falconpy/_endpoint/__init__.py
@@ -78,6 +78,7 @@ from ._admission_control_policies import _admission_control_policies_endpoints
 from ._agent_invocation import _agent_invocation_endpoints
 from ._agent_templates import _agent_templates_endpoints
 from ._agent_versions import _agent_versions_endpoints
+from ._agents import _agents_endpoints
 from ._alerts import _alerts_endpoints
 from ._api_clients import _api_clients_endpoints
 from ._api_integrations import _api_integrations_endpoints
@@ -212,6 +213,7 @@ api_endpoints.extend(_admission_control_policies_endpoints)
 api_endpoints.extend(_agent_invocation_endpoints)
 api_endpoints.extend(_agent_templates_endpoints)
 api_endpoints.extend(_agent_versions_endpoints)
+api_endpoints.extend(_agents_endpoints)
 api_endpoints.extend(_alerts_endpoints)
 api_endpoints.extend(_api_clients_endpoints)
 api_endpoints.extend(_api_integrations_endpoints)
